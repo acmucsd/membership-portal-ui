e709ca3feb9374410a40ec21502eefb5e815b72c
leestanley <stanleylee91776@gmail.com>
2019-07-23 22:55:27 -0700

    index

--- src/index.js
+++ src/index.js
@@ -4,11 +4,9 @@ import ReactDOM from 'react-dom';
 function App() {
   return (
     <div className="App">
-        <h1>Hello world!</h1>
+      <h1>Hello world!</h1>
     </div>
   );
 }
 
-
 ReactDOM.render(<App />, document.getElementById('root'));
-
