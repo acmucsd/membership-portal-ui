6e2d4a1a80abda96333d61eec823dad81dc73b34
leestanley <stanleylee91776@gmail.com>
2019-07-23 23:05:22 -0700

    AHH

--- prettier.config.js
+++ prettier.config.js
@@ -1,12 +1,12 @@
 module.exports = {
-    printWidth: 120,
-    tabWidth: 2,
-    useTabs: false,
-    semi: true,
-    singleQuote: true,
-    trailingComma: "es5",
-    bracketSpacing: true,
-    jsxBracketSameLine: true,
-    arrowParens: "avoid",
-    jsxSingleQuote:true
+  printWidth: 120,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  trailingComma: "es5",
+  bracketSpacing: true,
+  jsxBracketSameLine: true,
+  arrowParens: "avoid",
+  jsxSingleQuote:true
 };
