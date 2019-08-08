13423d9c7f186dc18b20c5b7bdd268d0c2895617
Antony Nguyen <emilyhuongnguyen@gmail.com>
2019-08-08 12:06:03 -0700

    Set up router (#16)

--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ConnectedRouter } from 'connected-react-router';
+import { Route, Switch } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
-import store from './store';
+import configureStore, { history } from './store';
+
+import HomePage from './containers/HomePage';
+
+const store = configureStore();
 
 const App = () => {
   return (
     <Provider store={store}>
-      <h1>Hello world!</h1>
+      <ConnectedRouter history={history}>
+        <>
+          <Switch>
+            <Route path='/' component={HomePage} />
+          </Switch>
+        </>
+      </ConnectedRouter>
     </Provider>
   );
 };
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
+import { connectRouter } from 'connected-react-router';
 
 import TempReducer from './TempReducer';
 
-export default combineReducers({
+export default (history) => combineReducers({
+  router: connectRouter(history),
   TempReducer
 });
