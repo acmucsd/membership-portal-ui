3073323bac0e60f04c4a667a401726e2a077bc4a
Antony Nguyen <emilyhuongnguyen@gmail.com>
2019-08-07 23:01:50 -0700

    Set up testing (#13)

---
+++ tests/setup/tests.test.js
@@ -0,0 +1,13 @@
+/**
+ * TODO - Delete this file.
+ * This temporary test file is only here for the purpose of showcasing testing.
+ * It will be deleted once the first test file is created.
+ */
+
+import Jest from 'jest';
+
+describe('Jest Tests', () => {
+  test('Verify Tests Work', () => {
+    expect(true).toBeTruthy();
+  });
+});
