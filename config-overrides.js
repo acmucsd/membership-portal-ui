0a964288979338bfab1f727e81072f7a9364dff8
leestanley <stanleylee91776@gmail.com>
2019-08-11 22:59:28 -0700

    finished event layout

--- config-overrides.js
+++ config-overrides.js
@@ -8,6 +8,9 @@ module.exports = override(
   }),
   addLessLoader({
     javascriptEnabled: true,
-    modifyVars: { '@primary-color': '#1DA57A' },
-  }),
+    modifyVars: {
+      '@primary-color': '#1DA57A',
+      '@success-color': '#22ACEA',
+    },
+  })
 );
--- src/event-check-in/index.jsx
+++ src/event-check-in/index.jsx
@@ -2,18 +2,18 @@ import React from 'react';
 import { Card, Input, Button } from 'antd';
 import './style.less';
 
-const EventCheck = (props) => {
-    return (
-    <div className="cards">
-        <Card className="card">
-            <h1>Event Check In</h1>
-            <Input placeholder='Attendance code...'/>
-            <Button className="submit">
-                Submit
-            </Button>
-        </Card>
+const EventCheck = props => {
+  return (
+    <div className='cards'>
+      <Card className='card'>
+        <h1>Event Check-in</h1>
+        <div className='inputbox'>
+          <Input className='input' size="large" placeholder='Attendance code...' />
+          <Button className='submit'>Submit</Button>
+        </div>
+      </Card>
     </div>
-    )
+  );
 };
 
 export default EventCheck;
