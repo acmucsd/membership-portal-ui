e3856a26382907aa6cd733de04d337e890c6e021
eminguyen <emilyhuongnguyen@gmail.com>
2019-08-22 23:58:15 -0700

    Rename VerticalNavItem to ListNavItem

--- src/components/VerticalNavBar/index.jsx
+++ src/components/VerticalNavBar/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 import './style.less';
-import VerticalNavItem from '../VerticalNavItem';
+import ListNavItem from '../ListNavItem';
 import { ReactComponent as ACMIcon } from '../../assets/graphics/acm-icon.svg';
 import { ReactComponent as DashboardIcon } from '../../assets/graphics/dashboard-icon.svg';
 import { ReactComponent as DiscordIcon } from '../../assets/graphics/discord-icon.svg';
@@ -16,26 +16,26 @@ export default () => {
       <section>
         <h1>Portal</h1>
         <NavLink activeClassName="selected" className="nav-item" to="/dashboard">
-          <VerticalNavItem icon={DashboardIcon} link="/dashboard" text="Dashboard" />
+          <ListNavItem icon={DashboardIcon} link="/dashboard" text="Dashboard" />
         </NavLink>
         <NavLink activeClassName="selected" className="nav-item" to="/leaderboard">
-          <VerticalNavItem icon={LBIcon} text="Leaderboard" />
+          <ListNavItem icon={LBIcon} text="Leaderboard" />
         </NavLink>
         <NavLink activeClassName="selected" className="nav-item" to="/profile">
-          <VerticalNavItem icon={ProfileIcon} text="Profile" />
+          <ListNavItem icon={ProfileIcon} text="Profile" />
         </NavLink>
       </section>
 
       <section>
         <h1>ACM</h1>
         <NavLink activeClassName="selected" className="nav-item" to="/about">
-          <VerticalNavItem icon={ACMIcon} text="About ACM" />
+          <ListNavItem icon={ACMIcon} text="About ACM" />
         </NavLink>
         <NavLink activeClassName="selected" className="nav-item" to="/swag">
-          <VerticalNavItem icon={SwagIcon} text="ACM Store" />
+          <ListNavItem icon={SwagIcon} text="ACM Store" />
         </NavLink>
         <NavLink activeClassName="selected" className="nav-item" to="https://discord.gg/mK9Zrf4d">
-          <VerticalNavItem icon={DiscordIcon} text="Discord" />
+          <ListNavItem icon={DiscordIcon} text="Discord" />
         </NavLink>
       </section>
     </div>
