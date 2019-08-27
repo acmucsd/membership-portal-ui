b84fa83b688bd733df9e3fba238a24eb1dc8019f
eminguyen <emilyhuongnguyen@gmail.com>
2019-08-27 00:23:28 -0700

    Restructure pages

---
+++ src/containers/LoginPage.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const LoginPage = () => {
+  return <h1>This is the login page.</h1>;
+};
+
+export default LoginPage;
---
+++ src/containers/StorePage.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const StorePage = () => {
+  return <h1>This is the store page.</h1>;
+};
+
+export default StorePage;
