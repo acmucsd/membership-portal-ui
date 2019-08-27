34c5cff368411f511c734a2f012a5bf62ff3dd53
Stanley Lee <stanleylee91776@gmail.com>
2019-08-26 17:59:47 -0700

    Event check in (Closes #6) (#34)
    * updated husky
    
    * layout finished
    
    * finished event layout
    
    * container
    
    * added less variables
    
    * removed sign in page
    
    * capital letters

---
+++ src/components/EventCheckIn/index.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Card, Input, Button } from 'antd';
+
+import './style.less';
+
+const EventCheck = props => {
+  return (
+    <div className="cards">
+      <Card className="card">
+        <h1>Event Check-in</h1>
+        <div className="inputbox">
+          <Input 
+            onChange={props.onChange} 
+            value={props.value}
+            className="input" 
+            size="large" 
+            placeholder="Attendance code..." 
+          />
+          <Button onClick={props.onClick} className="submit">
+            Submit
+          </Button>
+        </div>
+      </Card>
+    </div>
+  );
+***REMOVED***
+
+EventCheck.propTypes = {
+  onClick: PropTypes.func,
+  onChange: PropTypes.func,
+  value: PropTypes.string,
+***REMOVED***
+
+export default EventCheck;
