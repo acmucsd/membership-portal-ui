6034dc035d07b27c78fec5e8f27d6dbffb5330b1
Emily Nguyen <emilyhuongnguyen@gmail.com>
2019-08-28 01:32:22 -0700

    Add dropdown container and auth reducer (#59)
    * Add dropdown container
    
    * Change name to NavDropdown
    
    * Update links

---
+++ src/containers/NavDropdown.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+import NavDropdown from '../components/NavDropdown';
+import { logoutUser } from '../actions/authActions';
+
+const NavDropdownContainer = props => {
+  return <NavDropdown logout={props.logout} />;
+};
+
+const mapDispatchToProps = dispatch => ({
+  logout: () => {
+    dispatch(logoutUser());
+  },
+});
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(NavDropdownContainer);
