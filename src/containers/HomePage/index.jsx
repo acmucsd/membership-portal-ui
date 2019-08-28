7c1b45ca7300f4d66e27e0a4ae2f2cdfc142e2ae
Emily Nguyen <emilyhuongnguyen@gmail.com>
2019-08-27 23:05:30 -0700

    Layout page structure (#54)

--- src/containers/HomePage/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import EventCardContainer from '../EventCardContainer/EventCardContainer';
-
-const HomePage = () => {
-  return <h1>Hello World</h1>;
-};
-
-export default HomePage;
