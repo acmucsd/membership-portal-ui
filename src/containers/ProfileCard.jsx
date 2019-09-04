acdb5c20b61a90ba75322aaded6cdd33eedfe799
eminguyen <emilyhuongnguyen@gmail.com>
2019-09-04 07:48:39 -0700

    Fix styling for ProfileCard container

--- src/containers/ProfileCard.jsx
+++ src/containers/ProfileCard.jsx
@@ -9,7 +9,15 @@ const NavProfileContainer = props => {
     props.fetchUser();
   });
 
-  return <ProfileCard exp={props.exp} image={props.image} level={props.level} name={props.name} rank={props.rank} />;
+  return(
+    <ProfileCard
+      exp={props.exp}
+      image={props.image}
+      level={props.level}
+      name={props.name}
+      rank={props.rank}
+    />;
+  )
 };
 
 const mapStateToProps = state => ({
