b1bf4dfe619c17e5622f7b1a4c2b4e090b9a03ca
Emily Nguyen <emilyhuongnguyen@gmail.com>
2019-09-08 00:59:11 -0700

    Add profile card container (#67)

--- src/components/ProfileCard/index.jsx
+++ src/components/ProfileCard/index.jsx
@@ -6,36 +6,33 @@ import './style.less';
 
 const ProfileCard = props => {
   return (
-    <div onClick={props.onClick}>
-      <Card bordered={false} className="card">
-        <div className="avatar-flex">
-          <Avatar size={115} icon="user" className="avatar" src={props.src} />
+    <Card bordered={false} className="card">
+      <div className="avatar-flex">
+        <Avatar size={115} icon="user" className="avatar" src={props.image} />
+      </div>
+      <div className="info">
+        <div className="content">
+          <h2>{props.name}</h2>
+          <h3>{props.rank}</h3>
+          <Progress
+            successPercent={props.exp}
+            percent={100}
+            showInfo={false}
+            strokeWidth={12}
+            strokeColor="#587291"
+          />
+          <p>
+            <span> LEVEL {props.level}</span>
+            <span class="experience"> {props.exp} / 100 </span>
+          </p>
         </div>
-        <div className="info">
-          <div className="content">
-            <h2>{props.name}</h2>
-            <h3>{props.rank}</h3>
-            <Progress
-              successPercent={props.exp}
-              percent={100}
-              showInfo={false}
-              strokeWidth={12}
-              strokeColor="#587291"
-            />
-            <p>
-              <span> LEVEL {props.level}</span>
-              <span class="experience"> {props.exp} / 100 </span>
-            </p>
-          </div>
-        </div>
-      </Card>
-    </div>
+      </div>
+    </Card>
   );
 ***REMOVED***
 
 ProfileCard.propTypes = {
-  onClick: PropTypes.func,
-  src: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
   rank: PropTypes.string.isRequired,
   exp: PropTypes.number.isRequired,
