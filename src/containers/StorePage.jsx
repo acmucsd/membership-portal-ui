a416f6b7580f80d9a7aa6bf6891e682465d0d7b7
eminguyen <emilyhuongnguyen@gmail.com>
2019-09-08 18:46:03 -0700

    Add store page with coming soon icon

--- src/containers/StorePage.jsx
+++ src/containers/StorePage.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import PageLayout from '../components/PageLayout';
+import StorePage from '../components/StorePage';
 
-const StorePage = () => {
-  return <PageLayout>This is the store page.</PageLayout>;
+const StorePageContainer = () => {
+  return (
+    <PageLayout>
+      <StorePage />
+    </PageLayout>
+  );
 };
 
-export default StorePage;
+export default StorePageContainer;
