373ee7c40a6e87ce447b316ab3ba6ab0a73334d4
Emily Nguyen <emilyhuongnguyen@gmail.com>
2019-09-11 02:25:35 -0700

    Add store page with coming soon icon (#69)

---
+++ src/components/StorePage/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Icon } from 'antd';
+
+import './style.less';
+import { ReactComponent as SwagIcon } from '../../assets/icons/swag-icon.svg';
+
+const StorePage = props => {
+  return (
+    <div className="store-page">
+      <h1>ACM Store</h1>
+      <div className="coming-soon">
+        <Icon className="temp-icon" component={SwagIcon} />
+        <p>Coming Soon!</p>
+      </div>
+    </div>
+  );
+***REMOVED***
+
+export default StorePage;
