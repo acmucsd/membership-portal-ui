367abe1fc41a590bf17501d9f3ecb9b5aef376c2
Emily Nguyen <emilyhuongnguyen@gmail.com>
2019-09-12 11:47:24 -0700

    Add about page (#72)

--- src/containers/AboutPage.jsx
+++ src/containers/AboutPage.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import AboutPage from '../components/AboutPage';
 import PageLayout from '../components/PageLayout';
 
-const AboutPage = () => {
-  return <PageLayout>This is the about page.</PageLayout>;
+const AboutPageContainer = () => {
+  return (
+    <PageLayout>
+      <AboutPage />
+    </PageLayout>
+  );
 ***REMOVED***
 
-export default AboutPage;
+export default AboutPageContainer;
