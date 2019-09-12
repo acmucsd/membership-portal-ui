c8c8f031633d9d381d15ccd2731f1c6cc8574362
Emily Nguyen <emilyhuongnguyen@gmail.com>
2019-09-12 11:47:24 -0700

    Add about page (#72)

---
+++ src/config.js
@@ -0,0 +1,10 @@
+export default {
+  about: `With 100,000 members and 500+ chapters, the Association for Computing
+          Machinery is the world's largest society for computing. Here at UC
+          San Diego, our chapter has been established with the mission of
+          creating a member-first community devoted to the field of computing.
+          We welcome students of all backgrounds and skill levels to come
+          develop their skills at our many workshops and form new friendships at
+          our many socials. Get involved today by signing up for an event on
+          this portal or following us on social media!`
+}
