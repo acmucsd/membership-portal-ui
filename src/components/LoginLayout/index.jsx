6871e36947b7e40a188d78807e0c8917572e0fe0
leestanley <stanleylee91776@gmail.com>
2019-09-18 04:38:28 -0700

    layout for containers

---
+++ src/components/LoginLayout/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import './style.less';
+
+const LoginLayout = props => {
+  return <div className="contents">{props.children}</div>;
+};
+
+
+export default LoginLayout;
