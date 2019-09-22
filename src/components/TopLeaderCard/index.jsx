4193b1b47102ba7bf8ff113eaa4f5f2553c386bc
eminguyen <emilyhuongnguyen@gmail.com>
2019-09-22 09:34:09 -0700

    Update styling for TopLeaderBoardCard

--- src/components/TopLeaderCard/index.jsx
+++ src/components/TopLeaderCard/index.jsx
@@ -7,18 +7,20 @@ import './style.less';
 const TopLeaderCard = props => {
   return (
     <div>
-        <div className="leaderboard-card">
-          <div className={`leaderboard-card-left ${props.placement == 1 ? "first" : (props.placement == 2 ? "second" : "third")}`}>
-            <span className="placement">{props.placement}</span>
-          </div>
-          <div className="leaderboard-card-right">
+      <div className="leaderboard-card">
+        <div className={`leaderboard-card-left ${props.placement == 1 ? "first" : (props.placement == 2 ? "second" : "third")}`}>
+          <span className="placement">{props.placement}</span>
+        </div>
+        <div className="leaderboard-card-right">
+          <div>
             <Avatar size={81} src={props.imageSrc} />
-            <h1>{props.name}</h1>
-            <h3>{props.rank}</h3>
-            <h2>{props.exp} points</h2>
           </div>
+          <h1>{props.name}</h1>
+          <h3>{props.rank}</h3>
+          <h2>{props.exp} points</h2>
         </div>
       </div>
+    </div>
   );
 };
 
