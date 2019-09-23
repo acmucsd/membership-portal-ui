508645aeae3b85936e137360e6027dfd194db344
Emily Nguyen <emilyhuongnguyen@gmail.com>
2019-09-22 21:38:50 -0700

    Add dashboard to homepage (#82)

---
+++ src/components/HomePage/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import './style.less';
+import EventCheckIn from '../../containers/EventCheckIn';
+import ProfileCard from '../../containers/ProfileCard';
+
+const HomePage = () => {
+  return (
+    <div className="home-page">
+      <h1 className="title">Dashboard</h1>
+      <div className="dashboard">
+        <ProfileCard />
+        <EventCheckIn />
+        <hr className="border"></hr>
+      </div>
+    </div>
+  );
+};
+
+ export default HomePage;
