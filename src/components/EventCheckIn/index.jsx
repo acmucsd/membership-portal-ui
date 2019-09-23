07ff4e1f822e8daa5edc7f32c41a28a02f5facdf
eminguyen <emilyhuongnguyen@gmail.com>
2019-09-22 21:29:42 -0700

    Add dashboard to homepage

--- src/components/EventCheckIn/index.jsx
+++ src/components/EventCheckIn/index.jsx
@@ -6,23 +6,21 @@ import './style.less';
 
 const EventCheck = props => {
   return (
-    <div className="cards">
-      <Card className="card">
-        <h1>Event Check-in</h1>
-        <div className="inputbox">
-          <Input
-            onChange={props.onChange}
-            value={props.value}
-            className="input"
-            size="large"
-            placeholder="Attendance code..."
-          />
-          <Button onClick={props.onSubmit} className="submit">
-            Submit
-          </Button>
-        </div>
-      </Card>
-    </div>
+    <Card className="checkin-card">
+      <h1>Event Check-in</h1>
+      <div className="inputbox">
+        <Input
+          onChange={props.onChange}
+          value={props.value}
+          className="input"
+          size="large"
+          placeholder="Attendance code..."
+        />
+        <Button onClick={props.onSubmit} className="submit">
+          Submit
+        </Button>
+      </div>
+    </Card>
   );
 };
 
