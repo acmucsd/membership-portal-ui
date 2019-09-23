bf86bfd42adb29bbce5e22b980fc64511c1065e4
Emily Nguyen <emilyhuongnguyen@gmail.com>
2019-09-22 18:19:26 -0700

    Leaderboard Top Card Component (#78)

---
+++ src/components/TopLeaderCard/index.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Avatar } from 'antd';
+
+import './style.less';
+
+const TopLeaderCard = props => {
+  return (
+    <div>
+      <div className="leaderboard-card">
+        <div
+          className={`leaderboard-card-left
+          ${props.placement === 1 ? "first" :
+          (props.placement === 2 ? "second" : "third")}`}>
+          <span className="placement">{props.placement}</span>
+        </div>
+        <div className="leaderboard-card-right">
+          <div>
+            <Avatar size={81} src={props.imageSrc} />
+          </div>
+          <h1>{props.name}</h1>
+          <h3>{props.rank}</h3>
+          <h2>{props.exp} points</h2>
+        </div>
+      </div>
+    </div>
+  );
+***REMOVED***
+
+TopLeaderCard.propTypes = {
+  placement: PropTypes.number.isRequired,
+  name: PropTypes.string.isRequired,
+  rank: PropTypes.string.isRequired,
+  exp: PropTypes.number.isRequired,
+  imageSrc: PropTypes.string.isRequired
+***REMOVED***
+
+export default TopLeaderCard;
--- src/containers/LeaderPage.jsx
+++ src/containers/LeaderPage.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PageLayout from '../components/PageLayout';
+import TopLeaderCard from '../components/TopLeaderCard';
 
 const LeaderPage = () => {
   return <PageLayout>This is the leaderboard page.</PageLayout>;
