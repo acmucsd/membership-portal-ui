cae9b94defce8e9f5bb4ca237a0302e072783fcc
Emily Nguyen <emilyhuongnguyen@gmail.com>
2019-09-24 17:38:48 -0700

    Redirect to home page once authenticated (#94)

--- src/containers/SignInForm.jsx
+++ src/containers/SignInForm.jsx
@@ -2,7 +2,7 @@ import { connect } from 'react-redux';
 import { withFormik } from 'formik';
 
 import SignInForm from '../components/SignInForm';
-import { loginUser } from '../actions/authActions';
+import { loginUser, redirectHome } from '../actions/authActions';
 
 const FormikSignInForm = withFormik({
   mapPropsToValues() {
@@ -14,10 +14,11 @@ const FormikSignInForm = withFormik({
   handleSubmit(values, { resetForm, props }) {
     props.loginUser(values);
     resetForm();
-  }
+    props.redirectHome();
+  },
 })(SignInForm);
 
 export default connect(
   null,
-  { loginUser }
+  { loginUser, redirectHome}
 )(FormikSignInForm);
