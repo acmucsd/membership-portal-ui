f32c22557f8e728824c149886deb1c409c597dfb
eminguyen <emilyhuongnguyen@gmail.com>
2019-09-24 18:15:48 -0700

    Add admin access in navbar

--- src/containers/HomePage.jsx
+++ src/containers/HomePage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import HomePage from '../components/HomePage';
-import PageLayout from '../components/PageLayout';
+import PageLayout from './PageLayout';
 
 const HomePageContainer = () => {
   return (
