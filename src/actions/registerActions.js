0ac761d8967c77cd2b20c3e028ae1fbf803854b2
Stanley Lee <stanleylee91776@gmail.com>
2019-09-26 22:41:02 -0700

    Update registerActions.js

--- src/actions/registerActions.js
+++ src/actions/registerActions.js
@@ -2,7 +2,6 @@ import { REGISTER_USER } from './types';
 import { replace } from 'connected-react-router';
 
 export const registerAccount = (values) => dispatch => {
-  console.log(values)
   dispatch({
     type: REGISTER_USER,
     payload: values
