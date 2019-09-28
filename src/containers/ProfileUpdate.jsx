d5bc737ba3979409328b63cf86c6fae774a6bc14
leestanley <stanleylee91776@gmail.com>
2019-09-27 21:50:49 -0700

    styling

--- src/containers/ProfileUpdate.jsx
+++ src/containers/ProfileUpdate.jsx
@@ -5,23 +5,22 @@ import ProfileUpdate from '../components/ProfileUpdate';
 import { updateProfile } from '../actions/profileActions';
 
 const FormikProfileUpdate = withFormik({
-    mapPropsToValues() {
-      return {
-        firstName: '',
-        lastName: '',
-        graduationYear: 0,
-        major: '',
-        about: ''
-      }
-    },
-    handleSubmit(values, { resetForm, props }) {
-      props.updateProfile(values);
-      resetForm();
-    },
-  })(ProfileUpdate);
-  
-  export default connect(
-    null,
-    { updateProfile }
-  )(FormikProfileUpdate);
-  
+  mapPropsToValues() {
+    return {
+      firstName: '',
+      lastName: '',
+      graduationYear: 0,
+      major: '',
+      about: '',
+    };
+  },
+  handleSubmit(values, { resetForm, props }) {
+    props.updateProfile(values);
+    resetForm();
+  },
+})(ProfileUpdate);
+
+export default connect(
+  null,
+  { updateProfile }
+)(FormikProfileUpdate);
