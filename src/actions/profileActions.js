77c2f600584e97f3ca2606673b57b0bfd926c48d
Stanley Lee <stanleylee91776@gmail.com>
2019-09-28 10:01:20 -0700

    Profile Container (#110)
    * initial profile layout
    
    * profile update
    
    * added reducers and actions
    
    * finished container
    
    * removed required auth
    
    * styling
    
    * removing excess files

---
+++ src/actions/profileActions.js
@@ -0,0 +1,9 @@
+import { PROFILE_SUCCESS, PROFILE_FAIL } from './types';
+
+export const updateProfile = (values) => dispatch => {
+    dispatch({
+      type: PROFILE_SUCCESS,
+      payload: values
+    })
+  };
+  
