061c45fc0e0322a13f3e67da4d331596e7453758
Emily Nguyen <emilyhuongnguyen@gmail.com>
2019-09-27 20:19:34 -0700

    Connect sign in form to API (#107)

--- src/actions/authActions.js
+++ src/actions/authActions.js
@@ -1,11 +1,42 @@
-import { AUTH_USER, UNAUTH_USER } from './types';
+import { AUTH_ERROR, AUTH_USER, UNAUTH_USER } from './types';
 import { replace } from 'connected-react-router';
 
-export const loginUser = (values) => dispatch => {
-  // TODO - Log in the user here.
-  dispatch({
-    type: AUTH_USER
-  })
+import Config from '../config';
+
+export const loginUser = (values) => async dispatch => {
+  try {
+    const response = await fetch(Config.API_URL + Config.routes.auth.login, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        email: values.email,
+        password: values.password
+      }),
+    });
+
+    const data = await response.json();
+    if (!data) throw new Error('Empty response from server');
+    if (data.error) throw new Error(data.error.message);
+
+    // TODO: Store token in a cookie.
+    dispatch({
+      type: AUTH_USER,
+      // TODO: Check Admin Token
+      isAdmin: true
+    });
+
+    // Redirect to home on login.
+    dispatch(replace('/'));
+  }
+  catch(error) {
+    dispatch({
+      type: AUTH_ERROR,
+      error: error
+    });
+  }
 };
 
 export const logoutUser = () => dispatch => {
@@ -13,8 +44,4 @@ export const logoutUser = () => dispatch => {
   dispatch({
     type: UNAUTH_USER
   })
-};
-
-export const redirectHome= () => dispatch => {
-  dispatch(replace('/'));
 };
--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -1,17 +1,23 @@
-import { AUTH_USER, UNAUTH_USER } from '../actions/types';
+import { AUTH_ERROR, AUTH_USER, UNAUTH_USER } from '../actions/types';
 
 const initialState = {
   admin: false,
-  authenticated: false
+  authenticated: false,
+  error: false
 };
 
 const AuthReducer = (state = initialState, action) => {
   switch (action.type) {
+    case AUTH_ERROR:
+      return {
+        ...state,
+        error: action.error
+      };
     // TODO Authentificated should not always be true.
     case AUTH_USER:
       return {
         ...state,
-        admin: true,
+        admin: action.isAdmin,
         authenticated: true
       };
     case UNAUTH_USER:
