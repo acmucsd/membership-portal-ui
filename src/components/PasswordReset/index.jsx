68903d50c0dbe74ecfac0e3df73e510997a7b076
Stanley Lee <stanleylee91776@gmail.com>
2019-10-07 16:17:45 -0700

    Password Reset Layout (#138)
    * password reset components
    
    * Update style.less
    
    * reorganized css
    
    * Update index.jsx
    
    * Update index.jsx

---
+++ src/components/PasswordReset/index.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Form, Input } from 'antd';
+
+import './style.less';
+import logo from '../../assets/graphics/logo.svg';
+
+const PasswordForm = () => {
+  return (
+    <div className="resetcard">
+      <img src={logo} className="logo" alt="logo" />
+      <form>
+        <Form.Item className="code" label="Reset your password">
+          <Input
+            name="code"
+            type="text"
+            placeholder="Activation code"
+            className="input-box"
+            // value={props.values.email}
+            // onChange={props.handleChange}
+            // onBlur={props.handleBlur}
+          />
+        </Form.Item>
+        <Form.Item className="password">
+          <Input
+            name="password"
+            type="password"
+            placeholder="Password"
+            className="input-box"
+            // value={props.values.email}
+            // onChange={props.handleChange}
+            // onBlur={props.handleBlur}
+          />
+        </Form.Item>
+        <Form.Item className="password">
+          <Input
+            name="confirmpassword"
+            type="password"
+            placeholder="Confirm Password"
+            className="input-box"
+            // value={props.values.password}
+            // onChange={props.handleChange}
+            // onBlur={props.handleBlur}
+          />
+        </Form.Item>
+      </form>
+    </div>
+  );
+***REMOVED***
+
+PasswordForm.propTypes = {
+  handleBlur: PropTypes.func,
+  handleChange: PropTypes.func,
+  handleSubmit: PropTypes.func,
+  values: PropTypes.object,
+***REMOVED***
+
+export default PasswordForm;
---
+++ src/containers/PasswordUpdate.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import LoginLayout from '../components/LoginLayout';
+import PasswordReset from '../components/PasswordReset';
+
+const PasswordUpdate = () => {
+  return (
+    <LoginLayout>
+      <PasswordReset />
+    </LoginLayout>
+  );
+***REMOVED***
+
+export default PasswordUpdate;
