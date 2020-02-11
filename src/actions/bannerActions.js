c0af2801ddebbefb51644d9a8eea0865941ad284
StoneT2000 <stoneztao@gmail.com>
2020-02-11 01:04:44 -0800

    Revert

--- src/actions/bannerActions.js
+++ src/actions/bannerActions.js
@@ -13,21 +13,20 @@ const config = {
 };
 
 firebase.initializeApp(config);
-export const fetchBanner = () => dispatch => {};
-//
-// const storage = firebase.storage();
-//
-// export const fetchBanner = () => dispatch => {
-//   storage
-//     .ref(`/banner.png`)
-//     .getDownloadURL()
-//     .then(url =>
-//       dispatch({
-//         type: FETCH_BANNER,
-//         payload: url,
-//       })
-//     )
-//     .catch(err => {
-//       console.error('Failed to load banner image');
-//     });
-// };
+
+const storage = firebase.storage();
+
+export const fetchBanner = () => dispatch => {
+  storage
+    .ref(`/banner.png`)
+    .getDownloadURL()
+    .then(url =>
+      dispatch({
+        type: FETCH_BANNER,
+        payload: url,
+      })
+    )
+    .catch(err => {
+      console.error('Failed to load banner image');
+    });
+};
