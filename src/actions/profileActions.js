1f52b1d08cbb9c585f2548d079fe5362d49f7c17
Stone Tao <stoneztao@gmail.com>
2020-02-12 11:12:14 -0800

    Edit Profile Page updated (#162)
    * ProfileUpdate form is auto filled in with current profile data
    
    * Update profile action added + notifier
    
    * Mobile Responsiveness added

--- src/actions/profileActions.js
+++ src/actions/profileActions.js
@@ -1,9 +1,36 @@
 import { PROFILE_SUCCESS, PROFILE_FAIL } from './types';
+import Config from '../config';
+import Storage from '../storage';
+import { logoutUser } from './authActions';
+import { notify } from '../utils';
 
-export const updateProfile = (values) => dispatch => {
+export const updateProfile = (values) => async dispatch => {
+  try {
+    const response = await fetch(Config.API_URL + Config.routes.user.user, {
+      method: 'PATCH',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${Storage.get('token')}`,
+      },
+      body: JSON.stringify({ user: values }),
+***REMOVED***
+
+    const status = await response.status;
+    if (status === 401 || status === 403) {
+      dispatch(logoutUser());
+      return;
+    }
+    notify('Updated profile!', 'Just now');
     dispatch({
       type: PROFILE_SUCCESS,
       payload: values
     })
-  ***REMOVED***
-  
+  } catch(error) {
+    notify('Unable to update profile!', error.message);
+    dispatch({
+      type: PROFILE_FAIL,
+      payload: error.message,
+***REMOVED***
+  }
+***REMOVED***
