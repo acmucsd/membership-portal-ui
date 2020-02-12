93e7465d6bdf8ea4aacd3a0cde593566a0af0eed
Stone Tao <stoneztao@gmail.com>
2020-02-12 11:11:45 -0800

    No public editing allowed (#163)
    * Update index.jsx
    
    * Remove console.logs

--- src/components/EventCard/index.jsx
+++ src/components/EventCard/index.jsx
@@ -7,9 +7,6 @@ import './styles.less';
 
 const EventCard = (props) => {
   const history = useHistory();
-  useEffect(() => {
-    console.log(props.auth);
-  }, [props.auth]);
   return (
     <div className="event-card">
       <img className="image" src={props.cover} alt={props.title}/>
@@ -30,11 +27,13 @@ const EventCard = (props) => {
         />
         <h2 className="points">{props.points}</h2>
       </div>
-      <div className="edit-icon-wrapper">
-        <Icon type="edit" className='edit-icon' onClick={() => {
-          history.push("/admin/editEvent/" + props.uuid);
-        }}/>
-      </div>
+      {props.auth.admin &&
+        <div className="edit-icon-wrapper">
+          <Icon type="edit" className='edit-icon' onClick={() => {
+            history.push("/admin/editEvent/" + props.uuid);
+          }}/>
+        </div>
+      }
       <hr className="divider"/>
       <p className="description">{props.description}</p>
     </div>
