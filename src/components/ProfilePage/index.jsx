6a2c231f47d426f171991e665f22a19af0d8a643
StoneT2000 <stoneztao@gmail.com>
2020-03-19 12:26:53 -0700

    Profile Image finished

--- src/components/ProfilePage/index.jsx
+++ src/components/ProfilePage/index.jsx
@@ -3,19 +3,22 @@ import { Button, Avatar, Progress } from 'antd';
 import { useHistory, useParams, Link } from 'react-router-dom';
 import { getLevel, getRank } from '../../utils';
 import { fetchUserByID } from '../../actions/userActions';
-
+import { getDefaultProfile } from '../../utils';
 import './style.less';
 
 const ProfilePage = props => {
   const params = useParams();
   const history = useHistory();
-  const [user, setUser] = useState(props.user);
+  const [user, setUser] = useState("");
   useEffect(() => {
     if (params.uuid) {
       fetchUserByID(params.uuid).then((res) => {
         console.log({ profile:{...res.user}, image: res.user.profilePicture });
         setUser({ profile:{...res.user}, image: res.user.profilePicture });
       })
+    }
+    else {
+      setUser(props.user);
     }
   }, [params.uuid]);
   return (
@@ -26,7 +29,7 @@ const ProfilePage = props => {
         <div>
         <div className="avatar-flex">
           <h2 className="name">{ user.profile.firstName } { user.profile.lastName }</h2>
-          <Avatar size={115} icon="user" className="avatar" src={user.image} />
+          <Avatar size={115} icon="user" className="avatar" src={user.image || getDefaultProfile() } />
         </div>
         <div className="level-info">
           <p className="rank">{getRank(user.profile.points)}</p>
