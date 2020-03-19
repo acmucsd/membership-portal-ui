04f737958b014c9a898fa8cff7d3d087fe4b419b
StoneT2000 <stoneztao@gmail.com>
2020-03-19 12:17:32 -0700

    Simple Profile page done, can click user card on leaderboard to see other profile

--- src/components/ProfilePage/index.jsx
+++ src/components/ProfilePage/index.jsx
@@ -1,46 +1,61 @@
 import React, { useEffect, useState } from 'react';
 import { Button, Avatar, Progress } from 'antd';
-
-import { useHistory, useParams } from 'react-router-dom';
+import { useHistory, useParams, Link } from 'react-router-dom';
 import { getLevel, getRank } from '../../utils';
+import { fetchUserByID } from '../../actions/userActions';
 
 import './style.less';
 
 const ProfilePage = props => {
   const params = useParams();
   const history = useHistory();
+  const [user, setUser] = useState(props.user);
   useEffect(() => {
     if (params.uuid) {
-
+      fetchUserByID(params.uuid).then((res) => {
+        console.log({ profile:{...res.user}, image: res.user.profilePicture });
+        setUser({ profile:{...res.user}, image: res.user.profilePicture });
+      })
     }
-  }, []);
+  }, [params.uuid]);
   return (
     <div className="Profile-Page">
       <h1 className="title">Profile</h1>
 
-      <div className="avatar-flex">
-        <h2 className="name">{ props.user.profile.firstName } { props.user.profile.lastName }</h2>
-        <Avatar size={115} icon="user" className="avatar" src={props.user.image} />
-      </div>
-      <div className="level-info">
-        <p className="rank">{getRank(props.user.profile.points)}</p>
-        <Progress
-          successPercent={props.user.profile.points % 100}
-          percent={100}
-          showInfo={false}
-          strokeWidth={12}
-          strokeColor="#587291"
-        />
-        <p className="level-stats">
-          <span> LVL {getLevel(props.user.profile.points)}</span>
-          <span className="experience"> {props.user.profile.points % 100} / 100 </span>
-        </p>
-      </div>
-      <div className="meta-data">
-        <p>Graduation Year: {props.user.profile.graduationYear}</p>
-        <p>Major: {props.user.profile.major}</p>
-      </div>
-
+      {user && user.profile &&
+        <div>
+        <div className="avatar-flex">
+          <h2 className="name">{ user.profile.firstName } { user.profile.lastName }</h2>
+          <Avatar size={115} icon="user" className="avatar" src={user.image} />
+        </div>
+        <div className="level-info">
+          <p className="rank">{getRank(user.profile.points)}</p>
+          <Progress
+            successPercent={user.profile.points % 100}
+            percent={100}
+            showInfo={false}
+            strokeWidth={12}
+            strokeColor="#587291"
+          />
+          <p className="level-stats">
+            <span> LVL {getLevel(user.profile.points)}</span>
+            <span className="experience"> {user.profile.points % 100} / 100 </span>
+          </p>
+        </div>
+        <div className="meta-data">
+          <p>Graduation Year: {user.profile.graduationYear}</p>
+          <p>Major: {user.profile.major}</p>
+        </div>
+        {
+          !params.uuid &&
+          <Link to="/editProfile">
+            <Button className="edit-profile-btn">
+              Edit Profile
+            </Button>
+          </Link>
+        }
+        </div>
+      }
     </div>
   );
 };
