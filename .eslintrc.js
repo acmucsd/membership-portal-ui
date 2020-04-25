90af980027f907b07a207a52dc0c7abcb7ab4972
Paul Pan <panjunhong05@gmail.com>
2020-04-24 20:36:29 -0700

    Integrated eslint with prettier

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports =  {
+  extends:  [
+    'plugin:prettier/recommended',
+  ],
+  parserOptions:  {
+    ecmaVersion:  2018,  // Allows for the parsing of modern ECMAScript features
+    sourceType:  'module',  // Allows for the use of imports
+  },
+};
