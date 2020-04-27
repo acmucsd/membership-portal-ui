a7e19d3c2dcd837712151e394585e8b0a541feec
Stone Tao <stonezt2019@gmail.com>
2020-04-27 14:21:59 -0700

    Secret bread (#206)
    * Secret bread
    
    * lint

---
+++ src/containers/egg/BreadPage/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import LoginLayout from '../../../components/LoginLayout';
+import './style.less';
+const BreadPageContainer = () => {
+  // jank iframes
+  return (
+    <div className="BreadPage">
+      <h1>ACM Bread</h1>
+      <br />
+      <div className="iframe-container">
+        <iframe className="BreadPageFrame" src="https://stonet2000.github.io/Cut-Bread/"></iframe>
+      </div>
+    </div>
+  );
+***REMOVED***
+
+export default BreadPageContainer;
