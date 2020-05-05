28cb121a0a532d8b9e91cfb048fe80eb2018b127
Steven Steiner <steets250@gmail.com>
2020-05-04 21:40:50 -0700

    Require UUID + Cleaning

--- src/components/LeaderListItem/index.jsx
+++ src/components/LeaderListItem/index.jsx
@@ -13,16 +13,13 @@ const LeaderListItem = props => {
       ${props.placement % 2 === 0 ? 'even' : 'odd'}`}>
       <span className="placement">{props.placement}</span>
       <Avatar size={40} src={props.image} />
-
       <span className="column left">
         <div className="name">
           <Link to={'/profile/' + props.uuid}>{props.name}</Link>
         </div>
         <div className="rank mobile">{getRank(props.exp)}</div>
       </span>
-
       <span className="rank desktop">{getRank(props.exp)}</span>
-
       <span className="column right">
         <div className="exp">{props.exp} points</div>
       </span>
@@ -34,6 +31,7 @@ LeaderListItem.propTypes = {
   exp: PropTypes.number.isRequired,
   name: PropTypes.string.isRequired,
   placement: PropTypes.number.isRequired,
+  uuid: PropTypes.string.isRequired,
 };
 
 export default LeaderListItem;
