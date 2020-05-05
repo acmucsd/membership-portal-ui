b3691d686e3f705208ee221f080190b3522a9d3f
Steven Steiner <steets250@gmail.com>
2020-05-05 12:12:02 -0700

    Make Online Locations Clickable (#233)
    * Clickable Event Locations
    
    * Use Ternary

--- src/components/EventCard/index.jsx
+++ src/components/EventCard/index.jsx
@@ -3,17 +3,26 @@ import PropTypes from 'prop-types';
 import React, { useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 
+import { isURL, getAbsoluteURL } from '../../utils';
+
 import './styles.less';
 
 const EventCard = props => {
   const history = useHistory();
+
   return (
     <div className="event-card">
       <img className="image" src={props.cover} alt={props.title} />
       <div className="info">
         <h2 className="title">{props.title}</h2>
         <p className="date">{props.date}</p>
-        <p className="location">{props.location}</p>
+        {isURL(props.location) ? (
+          <a className="link" href={getAbsoluteURL(props.location)}>
+            <p className="location">{props.location}</p>
+          </a>
+    ***REMOVED*** : (
+          <p className="location">{props.location}</p>
+    ***REMOVED***}
       </div>
       <div className="circle">
         <div className="inner" />
--- src/utils.js
+++ src/utils.js
@@ -137,3 +137,35 @@ export const getMonthIndex = month => {
 export const getLevel = points => {
   return Math.floor(points / 100) + 1;
 ***REMOVED***
+
+/**
+ * Determines if given string is a valid website link.
+ * @param {string} str The string containing a potential URL.
+ * @return {boolean} True if valid URL, false otherwise.
+ */
+export const isURL = str => {
+  var pattern = new RegExp(
+    '^(https?:\\/\\/)?' + // protocol
+    '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
+    '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
+    '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
+    '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
+      '(\\#[-a-z\\d_]*)?$', // fragment locator
+    'i'
+  );
+
+  return !!pattern.test(str);
+***REMOVED***
+
+/**
+ * Ensures a valid website link is an absolute path.
+ * @param {string} str The string containing a URL.
+ * @return {string} A guaranteed absolute path for the link.
+ */
+export const getAbsoluteURL = str => {
+  if (isURL(str) && !/^https?:\/\//i.test(str)) {
+    return 'http://' + str;
+  }
+
+  return str;
+***REMOVED***
