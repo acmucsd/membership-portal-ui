1795a697916680b57e0aaf06fbaa23567d2f6356
Steven Steiner <steets250@gmail.com>
2020-05-04 19:38:18 -0700

    localhost -> heroku (#221)

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 let apiurl = 'https://acmucsd-membership-portal-api.herokuapp.com';
 if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
-  apiurl = 'http://localhost:3000';
+  apiurl = 'https://acmucsd-portal-testing.herokuapp.com';
 }
 
 export default {
