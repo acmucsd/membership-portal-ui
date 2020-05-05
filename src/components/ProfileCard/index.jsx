a7f9522e17c157c37caa0e52298c1c42756f9fb8
Steven Steiner <steets250@gmail.com>
2020-05-04 22:05:44 -0700

    Clickable Profile Card (#226)

--- src/components/ProfileCard/index.jsx
+++ src/components/ProfileCard/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import { Avatar, Card, Progress } from 'antd';
 
 import './style.less';
@@ -7,28 +8,30 @@ import { getDefaultProfile } from '../../utils';
 
 const ProfileCard = props => {
   return (
-    <Card bordered={false} className="profile-card">
-      <div className="avatar-flex">
-        <Avatar size={115} icon="user" className="avatar" src={props.profilePicture} />
-      </div>
-      <div className="info">
-        <div className="content">
-          <h2>{props.name}</h2>
-          <h3>{props.rank}</h3>
-          <Progress
-            successPercent={props.exp % 100}
-            percent={100}
-            showInfo={false}
-            strokeWidth={12}
-            strokeColor="#587291"
-          />
-          <p>
-            <span> LVL {props.level}</span>
-            <span className="experience"> {props.exp % 100} / 100 </span>
-          </p>
+    <Link to="/profile">
+      <Card bordered={false} className="profile-card">
+        <div className="avatar-flex">
+          <Avatar size={115} icon="user" className="avatar" src={props.profilePicture} />
         </div>
-      </div>
-    </Card>
+        <div className="info">
+          <div className="content">
+            <h2>{props.name}</h2>
+            <h3>{props.rank}</h3>
+            <Progress
+              successPercent={props.exp % 100}
+              percent={100}
+              showInfo={false}
+              strokeWidth={12}
+              strokeColor="#587291"
+            />
+            <p>
+              <span> LVL {props.level}</span>
+              <span className="experience"> {props.exp % 100} / 100 </span>
+            </p>
+          </div>
+        </div>
+      </Card>
+    </Link>
   );
 };
 
