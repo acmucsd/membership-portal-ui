fcaf577b0b06fb92e6aead5a68a1730c1c456779
Steven Steiner <steets250@gmail.com>
2020-05-04 21:55:48 -0700

    Use Dynamic Leaderboard Layout (#225)
    * Format + Ready for Badges
    
    * Mobile Font Size
    
    * Require UUID + Cleaning

--- src/components/LeaderListItem/index.jsx
+++ src/components/LeaderListItem/index.jsx
@@ -13,13 +13,16 @@ const LeaderListItem = props => {
       ${props.placement % 2 === 0 ? 'even' : 'odd'}`}>
       <span className="placement">{props.placement}</span>
       <Avatar size={40} src={props.image} />
-
-      <span className="name">
-        <Link to={'/profile/' + props.uuid}>{props.name}</Link>
+      <span className="column left">
+        <div className="name">
+          <Link to={'/profile/' + props.uuid}>{props.name}</Link>
+        </div>
+        <div className="rank mobile">{getRank(props.exp)}</div>
       </span>
-
-      <span className="rank">{getRank(props.exp)}</span>
-      <span className="exp">{props.exp} points</span>
+      <span className="rank desktop">{getRank(props.exp)}</span>
+      <span className="column right">
+        <div className="exp">{props.exp} points</div>
+      </span>
     </div>
   );
 };
@@ -28,6 +31,7 @@ LeaderListItem.propTypes = {
   exp: PropTypes.number.isRequired,
   name: PropTypes.string.isRequired,
   placement: PropTypes.number.isRequired,
+  uuid: PropTypes.string.isRequired,
 };
 
 export default LeaderListItem;
