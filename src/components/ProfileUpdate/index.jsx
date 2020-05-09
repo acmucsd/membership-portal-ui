cc0d87f1f856f71d7939d4ad414587fda61d5ef8
Matei-Alexandru Gardus <matei@gard.us>
2020-05-08 19:36:59 -0700

    Fix graduation year value in update profile form
    - Previously, the ProfileUpdate component updated the
      graduation year from the properties, while not doing so for the
      state value. Add state set function for graduation year as well
      in order to allow for client-side change of value.

--- src/components/ProfileUpdate/index.jsx
+++ src/components/ProfileUpdate/index.jsx
@@ -71,9 +71,19 @@ const ProfileUpdate = (props) => {
       });
   };
   useEffect(() => {
+<<<<<<< HEAD
     const keys = ['firstName', 'lastName', 'major', 'bio'];
     keys.forEach((key) => {
       setFieldValue(key, user.profile[key]);
+||||||| parent of cfd8f0e... Fix graduation year value in update profile form
+    const keys = ['firstName', 'lastName', 'major', 'bio', 'graduationYear'];
+    keys.forEach(key => {
+      props.setFieldValue(key, props.user.profile[key]);
+=======
+    let keys = ['firstName', 'lastName', 'major', 'bio', 'graduationYear'];
+    keys.forEach(key => {
+      props.setFieldValue(key, props.user.profile[key]);
+>>>>>>> cfd8f0e... Fix graduation year value in update profile form
     });
     if (user.profile.graduationYear) {
       setGradYear(user.profile.graduationYear);
@@ -163,10 +173,20 @@ const ProfileUpdate = (props) => {
               <Select
                 value={gradYear}
                 className="year"
+<<<<<<< HEAD
                 onBlur={(value) => setFieldValue('graduationYear', value)}
                 onChange={(value) => setFieldValue('graduationYear', value)}
               >
                 {years.map((num) => (
+||||||| parent of cfd8f0e... Fix graduation year value in update profile form
+                onBlur={value => props.setFieldValue('graduationYear', value)}
+                onChange={value => props.setFieldValue('graduationYear', value)}>
+                {years.map(num => (
+=======
+                onBlur={value => { setGradYear(value); props.setFieldValue('graduationYear', value) }}
+                onChange={value => { setGradYear(value); props.setFieldValue('graduationYear', value) }}>
+                {years.map(num => (
+>>>>>>> cfd8f0e... Fix graduation year value in update profile form
                   <Option key={num} value={num}>
                     {num}
                   </Option>
