5cd349a901df1f3b5843420f489e17e3ae9657ff
Steven Steiner <steets250@gmail.com>
2020-05-11 05:52:03 -0700

    Ignore Redux Props and Niche Accesibility Checks

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,18 @@ module.exports = {
     node: true,
   },
   rules: {
+    'jsx-a11y/click-events-have-key-events': ['off'],
     'jsx-a11y/href-no-hash': ['off'],
+    'jsx-a11y/label-has-associated-control': ['off'],
+    'jsx-a11y/no-static-element-interactions': ['off'],
     'react/jsx-filename-extension': ['warn', { extensions: ['.js', '.jsx'] }],
   },
+  overrides: [
+    {
+      files: ['src/containers/**/*.jsx'],
+      rules: {
+        'react/prop-types': 0
+      }
+    }
+  ]
 };
