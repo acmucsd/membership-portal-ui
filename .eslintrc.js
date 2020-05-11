c78ca6abaa461572e228d9e6e998aec63568f4ad
Steven Steiner <steets250@gmail.com>
2020-05-11 02:01:26 -0700

    Initial Airbnb Configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,28 @@
 module.exports = {
-  extends: ['plugin:prettier/recommended'],
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-      modules: true,
-    },
-    ecmaVersion: 2018,
-    sourceType: 'module',
+  extends: ['airbnb', 'plugin:prettier/recommended', 'prettier/react'],
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    jest: true,
+    node: true,
+  },
+  rules: {
+    'jsx-a11y/href-no-hash': ['off'],
+    'react/jsx-filename-extension': ['warn', { extensions: ['.js', '.jsx'] }],
+    'max-len': [
+      'warn',
+      {
+        code: 200,
+        tabWidth: 2,
+        comments: 200,
+        ignoreComments: false,
+        ignoreTrailingComments: true,
+        ignoreUrls: true,
+        ignoreStrings: true,
+        ignoreTemplateLiterals: true,
+        ignoreRegExpLiterals: true,
+      },
+    ],
   },
 };
