6e43c51f3587080e5971c8568591bd984392af07
Steven Steiner <steets250@gmail.com>
2020-05-11 05:19:38 -0700

    Props Destructuring and Variable Rename

--- src/components/ProfilePage/index.jsx
+++ src/components/ProfilePage/index.jsx
@@ -8,54 +8,57 @@ import { fetchUserByID } from '../../actions/userActions';
 import './style.less';
 
 const ProfilePage = (props) => {
+  const { user } = props;
+
   const params = useParams();
-  const [user, setUser] = useState('');
+  const [stateUser, setUser] = useState('');
   useEffect(() => {
     if (params.uuid) {
       fetchUserByID(params.uuid).then((res) => {
         setUser({ profile: { ...res.user }, image: res.user.profilePicture });
       });
     } else {
-      setUser(props.user);
+      setUser(user);
     }
-  }, [params.uuid, props.user]);
+  }, [params.uuid, user]);
+
   return (
     <div className="Profile-Page">
       <h1 className="title">Profile</h1>
 
-      {user && user.profile && (
+      {stateUser && stateUser.profile && (
         <div>
           <div className="avatar-flex">
             <h2 className="name">
-              {user.profile.firstName} {user.profile.lastName}
+              {stateUser.profile.firstName} {stateUser.profile.lastName}
             </h2>
             <Avatar
               size={115}
               icon="user"
               className="avatar"
-              src={user.profile.profilePicture || getDefaultProfile()}
+              src={stateUser.profile.profilePicture || getDefaultProfile()}
             />
           </div>
           <div className="level-info">
-            <p className="rank">{getRank(user.profile.points)}</p>
+            <p className="rank">{getRank(stateUser.profile.points)}</p>
             <Progress
-              successPercent={user.profile.points % 100}
+              successPercent={stateUser.profile.points % 100}
               percent={100}
               showInfo={false}
               strokeWidth={12}
               strokeColor="#587291"
             />
             <p className="level-stats">
-              <span> LVL {getLevel(user.profile.points)}</span>
+              <span> LVL {getLevel(stateUser.profile.points)}</span>
               <span className="experience">
                 {' '}
-                {user.profile.points % 100} / 100{' '}
+                {stateUser.profile.points % 100} / 100{' '}
               </span>
             </p>
           </div>
           <div className="meta-data">
-            <p>Graduation Year: {user.profile.graduationYear}</p>
-            <p>Major: {user.profile.major}</p>
+            <p>Graduation Year: {stateUser.profile.graduationYear}</p>
+            <p>Major: {stateUser.profile.major}</p>
           </div>
           {!params.uuid && (
             <Link to="/editProfile">
@@ -66,6 +69,10 @@ const ProfilePage = (props) => {
       )}
     </div>
   );
+};
+
+ProfilePage.propTypes = {
+  user: PropTypes.object.isRequired,
 };
 
 export default ProfilePage;
