d2a737e2b6ac8887ea01583d0d99ebe5c53f8315
Steven Steiner <steets250@gmail.com>
2020-05-11 02:07:31 -0700

    No Max Line Length Check

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,19 +10,5 @@ module.exports = {
   rules: {
     'jsx-a11y/href-no-hash': ['off'],
     'react/jsx-filename-extension': ['warn', { extensions: ['.js', '.jsx'] }],
-    'max-len': [
-      'warn',
-      {
-        code: 200,
-        tabWidth: 2,
-        comments: 200,
-        ignoreComments: false,
-        ignoreTrailingComments: true,
-        ignoreUrls: true,
-        ignoreStrings: true,
-        ignoreTemplateLiterals: true,
-        ignoreRegExpLiterals: true,
-      },
-    ],
   },
 };
--- .prettierrc.js
+++ .prettierrc.js
@@ -1,5 +1,4 @@
 module.exports = {
-  printWidth: 120,
   singleQuote: true,
   trailingComma: 'es5',
 };
