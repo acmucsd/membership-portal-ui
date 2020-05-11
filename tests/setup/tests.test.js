6ec1f40d6bb7da0a144afc22a8237746ddf15f6f
Steven Steiner <steets250@gmail.com>
2020-05-11 05:02:25 -0700

    Jest Placeholder Fixes

--- tests/setup/tests.test.js
+++ tests/setup/tests.test.js
@@ -4,8 +4,6 @@
  * It will be deleted once the first test file is created.
  */
 
-import Jest from 'jest';
-
 describe('Jest Tests', () => {
   test('Verify Tests Work', () => {
     expect(true).toBeTruthy();
