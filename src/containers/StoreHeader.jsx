c1e4b38f1fbc8e036d811f77a9d2720c5671162a
Arsen <arsenohanyan@gmail.com>
2020-05-31 16:30:02 -0700

    Header ready with dummy props

---
+++ src/containers/StoreHeader.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+import StoreHeader from '../components/StoreHeader';
+
+const StoreHeaderContainer = (props) => {
+  const history = useHistory();
+  function handleGoBack() {
+    history.goBack();
+  }
+  const { titleType, titleText, hideCart } = props;
+  return (
+    <StoreHeader
+      // points={props.points}
+      points={10000}
+      handleGoBack={handleGoBack}
+      // cartItems={props.cartItems}
+      cartItems={3}
+      title={
+        titleType === 'nav' ? (
+          <NavTitle titleText={titleText} />
+        ) : (
+          <TextTitle titleText={titleText} />
+        )
+      }
+      hideCart={hideCart}
+    />
+  );
+};
+
+const TextTitle = (props) => {
+  const { titleText } = props;
+  return <h1 className="text-title">{titleText}</h1>;
+};
+
+const NavTitle = (props) => {
+  const history = useHistory();
+  const { titleText } = props;
+  return (
+    <button
+      type="button"
+      className="nav-title"
+      onClick={() => history.goBack()}
+    >
+      {titleText}
+    </button>
+  );
+};
+
+// const mapStateToProps = state => ({
+//     points: state.user.profile.points,
+//     cartItems: state.cart.items.length
+// });
+
+export default connect(null, null)(StoreHeaderContainer);
