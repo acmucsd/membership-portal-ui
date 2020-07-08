c79256285f5d70a545e04053ba40077987d70f2f
Matei-Alexandru Gardus <matei@gard.us>
2020-07-08 16:46:47 +0300

    Add search bar and tags for admin order
    Search bar and buttons use Ant Design. Sadly, it turns out
    that the search bar might not be able to have the search icon
    at the beginning, according to Ant Design. Will look into this
    further.
    
    Additionally, the tag button do not have the same colors as the
    design draft for the store. Particularly, the "Completed" tag
    cannot be green. For now, I've left them with sensible defaults.

--- src/components/AdminOrderPage/index.tsx
+++ src/components/AdminOrderPage/index.tsx
@@ -1,22 +1,49 @@
 import React from 'react';
-import { Button } from 'antd';
-
+import { Button, Input } from 'antd';
 import { history } from '../../store';
 
 import './style.less';
 
+const { Search } = Input;
+
 const AdminOrderPage: React.FC = () => {
   return (
-    <div className="store-header">
-      <h2 className="title">Orders</h2>
-      <Button
-        className="admin-redirect-button"
-        onClick={() => {
-          history.push('/store');
-        }}
-      >
-        <b>Store</b>
-      </Button>
+    <div className="admin-orders">
+      <div className="store-header">
+        <h2 className="title">Orders</h2>
+        <Button
+          className="admin-redirect-button"
+          onClick={() => {
+            history.push('/store');
+          }}
+        >
+          <b>Store</b>
+        </Button>
+      </div>
+      <div className="search-section">
+        <Search
+          placeholder="Search..."
+          size="large"
+          // We'll include this console statement for now.
+          // We need a placeholder for the search functionality.
+          // eslint-disable-next-line no-console
+          onSearch={(value) => console.log(`Search not implemented! Here's value anyway: ${value}`)}
+        />
+        <div className="button-array">
+          <Button
+            // For this button, we need to remove margin on the left, hence the wonky "first-button class"
+            className="tag-button first-button"
+            type="primary"
+            ghost
+            shape="round"
+          >
+            Completed
+          </Button>
+          <Button className="tag-button" type="danger" ghost shape="round">
+            Cancelled
+          </Button>
+        </div>
+      </div>
     </div>
   );
 };
