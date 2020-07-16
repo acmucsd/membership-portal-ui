b0f6456d4b1349be0ec15f3ed1b3a3969aa6e399
Matei-Alexandru Gardus <matei@gard.us>
2020-07-16 16:29:34 +0300

    Add modal and checkbox functionality to AdminOrderItem
    Modals and checkboxes now exist for each order item. They are essentially
    little forms that can be used to update the relevant properties in order
    items.
    
    Currently, console statements are used to test event handlers for the modal
    and checkbox. Proper actions will be used once state is implemented for the
    store.

--- src/components/AdminOrderItem/index.tsx
+++ src/components/AdminOrderItem/index.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Avatar, Row, Col } from 'antd';
+import React, { useState } from 'react';
+import { Avatar, Checkbox, Form, Input, Modal, Row, Col } from 'antd';
 import Icon from '@ant-design/icons';
 import { ReactComponent as CommentBoxEmpty } from '../../assets/icons/CommentBoxEmpty.svg';
 
@@ -17,10 +17,51 @@ interface AdminOrderItemProps {
 }
 
 const AdminOrderItem: React.FC<AdminOrderItemProps> = (props) => {
+  const [visible, setVisible] = useState(false);
+  const [note, setNote] = useState('');
+  const [fulfilled, setFulfilled] = useState(false);
+  const showNote = () => {
+    setVisible(true);
+  };
+
+  const closeNote = () => {
+    setVisible(false);
+  };
+
+  const handleOk = () => {
+    // For now, console statements. These will be removed
+    // when the API is implemented in actions.
+    // eslint-disable-next-line no-console
+    console.log(`Update note is called here! Contents: ${note}`);
+    setVisible(false);
+  };
+
   const { orderItem } = props;
   return (
     <Row align="middle" justify="center" className="order-item">
-      <Col span={2} />
+      <Modal title="Notes" visible={visible} onOk={handleOk} okText="Submit" onCancel={closeNote}>
+        <form>
+          <Form.Item className="note">
+            <Input.TextArea
+              name="note"
+              onChange={(event) => setNote(event.target.value)}
+              rows={4}
+              value={note}
+            />
+          </Form.Item>
+        </form>
+      </Modal>
+      <Col span={2}>
+        <div className="item-checkbox">
+          <Checkbox
+            onChange={(event) => {
+              setFulfilled(event.target.checked);
+              // eslint-disable-next-line no-console
+              console.log(`Fulfill change called! Current value: ${fulfilled}`);
+            }}
+          />
+        </div>
+      </Col>
       <Col span={3}>
         <div className="item-icon">
           <Avatar shape="square">{orderItem.itemName}</Avatar>
@@ -38,7 +79,7 @@ const AdminOrderItem: React.FC<AdminOrderItemProps> = (props) => {
       </Col>
       <Col span={2}>
         <div className="item-notes">
-          <Icon component={CommentBoxEmpty} />
+          <Icon onClick={showNote} component={CommentBoxEmpty} />
         </div>
       </Col>
     </Row>
