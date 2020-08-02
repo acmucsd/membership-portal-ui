06a5ae5b6ffab98faa6857703b35f380248bb0ad
Stone Tao <stonezt2019@gmail.com>
2020-08-02 13:05:02 -0700

    updates and move to container

--- src/components/StorePage/index.tsx
+++ src/components/StorePage/index.tsx
@@ -4,40 +4,12 @@ import StoreCollectionsContainer from '../../containers/StoreCollections';
 
 import './style.less';
 import CheckoutCart from '../../containers/store/CheckoutCart';
-import { ReactComponent as SwagIcon } from '../../assets/icons/swag-icon.svg';
-
-const initialStateCheckoutCartState = {
-  cart: {
-    items: [
-      {
-        name: 'Whole Wheat Bread',
-        quantity: 3,
-        id: 'abcdef',
-        price: 250,
-        image:
-          'https://cdn.sallysbakingaddiction.com/wp-content/uploads/2019/11/homemade-sandwich-bread-600x900.jpg',
-      },
-      {
-        name: 'Better Whole Wheat Bread',
-        quantity: 1,
-        id: 'defasd',
-        price: 4000,
-        image:
-          'https://cdn.sallysbakingaddiction.com/wp-content/uploads/2019/11/homemade-sandwich-bread-600x900.jpg',
-      },
-    ],
-  },
-};
 
 const StorePage: React.FC = () => {
   return (
     <div className="store-page">
       <h1>ACM Store</h1>
-      <div className="coming-soon">
-        <CheckoutCart store={initialStateCheckoutCartState} />
-        <Icon className="temp-icon" component={SwagIcon} />
-        <p>Coming Soon!</p>
-      </div>
+      <CheckoutCart />
       <h1>Diamond Outfitters</h1>
       <StoreCollectionsContainer />
     </div>
--- src/containers/store/CheckoutCart.tsx
+++ src/containers/store/CheckoutCart.tsx
@@ -2,8 +2,32 @@ import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import CheckoutCart from '../../components/StorePage/CheckoutCart';
 
+const initialStateCheckoutCartState = {
+  cart: {
+    items: [
+      {
+        name: 'Whole Wheat Bread',
+        quantity: 3,
+        id: 'abcdef',
+        price: 250,
+        image:
+          'https://cdn.sallysbakingaddiction.com/wp-content/uploads/2019/11/homemade-sandwich-bread-600x900.jpg',
+      },
+      {
+        name: 'Better Whole Wheat Bread',
+        quantity: 1,
+        id: 'defasd',
+        price: 4000,
+        image:
+          'https://cdn.sallysbakingaddiction.com/wp-content/uploads/2019/11/homemade-sandwich-bread-600x900.jpg',
+      },
+    ],
+  },
+};
+
 const CheckoutCartContainer = ({ store }) => {
   useEffect(() => {}, [store]);
+  store = initialStateCheckoutCartState;
   return <CheckoutCart store={store} />;
 };
 
