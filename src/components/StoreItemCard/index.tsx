a3e3d2853ba85d856e344a9c0ea7bc698fc2b813
Jennifer <32044772+Froggerson@users.noreply.github.com>
2020-09-17 19:57:43 -0700

    added Item Card Component
    there's a diamond and catbread image file too
    
    ---
    This commit was modified from the original, found at:
    https://github.com/acmucsd/membership-portal-ui/pull/288/commits/1ff907fb5f4e095d2b20a01efa312234717ff1ff
    
    This commit has been modified to:
    - move files to TypeScript
    - convert Diamond PNG to SVG
    - address minimal PR review comments
    
    Modified-by: Matei-Alexandru Gardus <matei@gard.us>

--- src/components/StoreItemCard/index.tsx
+++ src/components/StoreItemCard/index.tsx
@@ -1,9 +1,28 @@
 import React from 'react';
+import { ReactComponent as Diamond } from '../../assets/icons/Diamond.svg';
+import CatBread from '../../assets/icons/cat_bread.jpg';
 
 import './style.less';
 
 const ItemCard: React.FC = () => {
-  return <div className="item-card">This is a placeholder.</div>;
+  return (
+    <div className="item-card">
+      <div className="inside-rectangle">
+        <div className="rectangle top" />
+        <div className="rectangle bottom" />
+        <div className="image-container">
+          <img className="image" src={CatBread} alt="Lorem Ipsum" />
+        </div>
+      </div>
+      <div className="info">
+        <p className="title">Cat Bread</p>
+        <div className="price">
+          <Diamond />
+          <p className="amount">5,000</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ItemCard;
