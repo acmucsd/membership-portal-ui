9ff55ee1755a29729c9afcfa39481be1bf2e4c52
Steven Steiner <steets250@gmail.com>
2020-10-18 01:22:01 -0700

    Submission Validation

--- src/containers/admin/CreateEventForm.tsx
+++ src/containers/admin/CreateEventForm.tsx
@@ -3,6 +3,7 @@ import { withFormik } from 'formik';
 
 import CreateEventForm from '../../components/admin/CreateEventForm';
 import { postEvent, copyLink } from '../../actions/adminActions';
+import { notify } from '../../utils';
 
 const FormikCreateEventForm = withFormik({
   mapPropsToValues() {
@@ -22,6 +23,61 @@ const FormikCreateEventForm = withFormik({
   },
   handleSubmit(values, { resetForm, props }: { [key: string]: any }) {
     const { startDate, startTime, endDate, endTime } = values;
+
+    if (values.title === '') {
+      notify('Event Creation Error', 'Title is required.');
+      return;
+    }
+
+    if (values.location === '') {
+      notify('Event Creation Error', 'Location is required.');
+      return;
+    }
+
+    if (values.pointValue === 0) {
+      notify('Event Creation Error', 'Points is required.');
+      return;
+    }
+
+    if (!startDate) {
+      notify('Event Creation Error', 'Start Date is required.');
+      return;
+    }
+
+    if (!startTime) {
+      notify('Event Creation Error', 'Start Time is required.');
+      return;
+    }
+
+    if (!endDate) {
+      notify('Event Creation Error', 'End Date is required.');
+      return;
+    }
+
+    if (!endTime) {
+      notify('Event Creation Error', 'End Time is required.');
+      return;
+    }
+
+    if (values.cover === '') {
+      notify('Event Creation Error', 'Cover is required.');
+      return;
+    }
+
+    if (values.description === '') {
+      notify('Event Creation Error', 'Description is required.');
+      return;
+    }
+
+    if (values.attendanceCode === '') {
+      notify('Event Creation Error', 'Attendance Code is required.');
+      return;
+    }
+
+    if (values.committee === '') {
+      notify('Event Creation Error', 'Community is required.');
+      return;
+    }
 
     const event = {
       title: values.title,
--- src/containers/admin/EditEventForm.tsx
+++ src/containers/admin/EditEventForm.tsx
@@ -3,6 +3,7 @@ import { withFormik } from 'formik';
 
 import EditEventform from '../../components/admin/EditEventForm';
 import { editEvent, deleteEvent, copyLink } from '../../actions/adminActions';
+import { notify } from '../../utils';
 
 const FormikEditEventForm = withFormik({
   mapPropsToValues() {
@@ -23,6 +24,61 @@ const FormikEditEventForm = withFormik({
   },
   handleSubmit(values, { props }: { [key: string]: any }) {
     const { startDate, startTime, endDate, endTime } = values;
+
+    if (values.title === '') {
+      notify('Event Edit Error', 'Title is required.');
+      return;
+    }
+
+    if (values.location === '') {
+      notify('Event Edit Error', 'Location is required.');
+      return;
+    }
+
+    if (values.pointValue === 0) {
+      notify('Event Edit Error', 'Points is required.');
+      return;
+    }
+
+    if (!startDate) {
+      notify('Event Edit Error', 'Start Date is required.');
+      return;
+    }
+
+    if (!startTime) {
+      notify('Event Edit Error', 'Start Time is required.');
+      return;
+    }
+
+    if (!endDate) {
+      notify('Event Edit Error', 'End Date is required.');
+      return;
+    }
+
+    if (!endTime) {
+      notify('Event Edit Error', 'End Time is required.');
+      return;
+    }
+
+    if (values.cover === '') {
+      notify('Event Edit Error', 'Cover is required.');
+      return;
+    }
+
+    if (values.description === '') {
+      notify('Event Edit Error', 'Description is required.');
+      return;
+    }
+
+    if (values.attendanceCode === '') {
+      notify('Event Edit Error', 'Attendance Code is required.');
+      return;
+    }
+
+    if (values.committee === '') {
+      notify('Event Edit Error', 'Community is required.');
+      return;
+    }
 
     const event = {
       uuid: values.uuid,
