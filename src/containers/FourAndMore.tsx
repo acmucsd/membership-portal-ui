9755fd31676e4bd9a2deca0a654d9354e35b12e0
Steven Steiner <steets250@gmail.com>
2021-01-12 15:31:32 -0800

    earliestDate in seconds

--- src/containers/FourAndMore.tsx
+++ src/containers/FourAndMore.tsx
@@ -51,7 +51,7 @@ const FourAndMoreContainer: React.FC<FourAndMoreContainerProps> = (props) => {
   const [timeframe, setTimeframe] = useState('All Time');
   const [startTime, setStartTime] = useState(0);
   const [endTime, setEndTime] = useState(0);
-  const earliestDate = new Date('Jun 15, 2019').valueOf();
+  const earliestDate = new Date('Jun 15, 2019').valueOf() / 1000;
   const yearInSeconds = 31536000;
 
   const hasMore = () => {
