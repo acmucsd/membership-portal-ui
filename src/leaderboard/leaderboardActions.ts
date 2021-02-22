3459452380b48579cb90f78987daabcf96d0b570
seanye24 <seanye24@gmail.com>
2021-02-22 00:58:50 -0800

    Added generic api handler (#416)
    * Add type declarations, generic fetch types
    
    * Add generic fetch api handler
    
    * Refactor api calls with fetchService

--- src/leaderboard/leaderboardActions.ts
+++ src/leaderboard/leaderboardActions.ts
@@ -2,30 +2,16 @@ import { FETCH_LEADERBOARD, LEADERBOARD_ERROR, ThunkActionCreator } from './lead
 import { logoutUser } from '../auth/authActions';
 
 import Config from '../config';
-import Storage from '../storage';
+import { fetchService } from '../utils';
 
 const fetchLeaderboard: ThunkActionCreator = (offset: number = 0, limit: number, from?: number, to?: number) => async (dispatch) => {
   try {
-    const response = await fetch(
-      `${Config.API_URL}${Config.routes.leaderboard}?offset=${offset}&limit=${limit}${from ? `&from=${from}` : ''}${to ? `&to=${to}` : ''}`,
-      {
-        method: 'GET',
-        headers: {
-          Accept: 'application/json',
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${Storage.get('token')}`,
-        },
-      },
-    );
+    const url = `${Config.API_URL}${Config.routes.leaderboard}?offset=${offset}&limit=${limit}${from ? `&from=${from}` : ''}${to ? `&to=${to}` : ''}`;
+    const data = await fetchService(url, 'GET', 'json', {
+      requiresAuthorization: true,
+      onFailCallback: () => dispatch(logoutUser()),
+    });
 
-    const { status } = response;
-    if (status === 401 || status === 403) {
-      dispatch(logoutUser());
-    }
-
-    const data = await response.json();
-    if (!data) throw new Error('Empty response from server');
-    if (data.error) throw new Error(data.error.message);
     dispatch({
       type: FETCH_LEADERBOARD,
       payload: data.leaderboard,
