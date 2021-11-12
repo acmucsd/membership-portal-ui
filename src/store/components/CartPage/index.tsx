251e63d50bf7b6bc7918671370f84c4edc5f4aee
Sean Ye <seanye24@gmail.com>
2021-11-11 18:51:25 -0800

    Add CartPage component

--- src/store/components/CartPage/index.tsx
+++ src/store/components/CartPage/index.tsx
@@ -1,15 +1,55 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Button, Typography } from 'antd';
 
-import CartDisplay from '../CartDisplay';
 import NavigationBar from '../NavigationBar';
-
+import CartDisplay from '../CartDisplay';
+import { MerchandiseItemOptionModel } from '../../../types';
+import { ReactComponent as DiamondIcon } from '../../../assets/icons/diamond-icon.svg';
 import './style.less';
 
-const CartPage = () => {
+type Cart = {
+  item: MerchandiseItemOptionModel;
+  quantity: number;
+}[];
+
+type CartPageProps = {
+  cart: Cart;
+  verifyCart: (onFail: () => void) => void;
+};
+const CartPage: React.FC<CartPageProps> = ({ cart, verifyCart }) => {
+  const [isCheckoutLocked, setIsCheckoutLocked] = useState(false);
+
+  // unlock checkout if cart is modified
+  useEffect(() => {
+    setIsCheckoutLocked(false);
+  }, [cart]);
+
+  const getTotal = (items: Cart) => items.reduce((sum, { item: { price }, quantity }) => sum + price * quantity, 0);
+
+  const renderTotalPrice = (total: number) => {
+    return (
+      <div className="total-price-container">
+        <Typography className="total-price-label">Total:</Typography>
+        <DiamondIcon className="total-price-icon" />
+        <Typography className="total-price">{total.toLocaleString()}</Typography>
+      </div>
+    );
+  };
+
+  const onCheckoutButtonClick = () => {
+    verifyCart(() => setIsCheckoutLocked(true));
+  };
+
   return (
-    <div className="cart-page">
+    <div className="cart-page-container">
       <NavigationBar />
-      <CartDisplay items={[]} />
+      <div className="cart-page">
+        <CartDisplay items={cart} />
+        {renderTotalPrice(getTotal(cart))}
+        <Button className="checkout-button" disabled={isCheckoutLocked} onClick={onCheckoutButtonClick}>
+          Checkout
+        </Button>
+      </div>
     </div>
   );
 };
