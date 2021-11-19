bed5df09da69d385b95c98d9ffdfc4882cc9a4d0
Paul Pan <panjunhong05@gmail.com>
2021-11-18 18:42:11 -0800

    Bind store to props in CheckoutPage.

--- src/store/components/CheckoutPage/index.tsx
+++ src/store/components/CheckoutPage/index.tsx
@@ -1,14 +1,21 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import CartDisplay from '../CartDisplay';
 import NavigationBar from '../NavigationBar';
 
-const CheckoutPage: React.FC = () => {
+const CheckoutPage: React.FC = (props) => {
   return (
     <div className="cart-page">
+      {JSON.stringify(props)}
       <NavigationBar />
       <CartDisplay items={[]} />
     </div>
   );
 };
 
-export default CheckoutPage;
+const mapStateToProps = (state: { [key: string]: any }) => ({
+  error: state.store.error,
+  cart: state.store.cart,
+});
+
+export default connect(mapStateToProps)(CheckoutPage);
