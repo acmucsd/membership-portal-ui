693200b3d71c6ab9676702d7bd98415c556aea05
Steven Steiner <steets250@gmail.com>
2021-12-07 17:16:00 -0800

    StoreButton

--- src/store/components/StoreButton/index.tsx
+++ src/store/components/StoreButton/index.tsx
@@ -3,14 +3,18 @@ import React from 'react';
 import './style.less';
 
 interface StoreButtonProps {
+  type?: 'primary' | 'secondary' | 'danger';
+  size?: 'small' | 'medium' | 'large';
   text: string;
-  onClick: React.MouseEventHandler<HTMLButtonElement>;
+  disabled?: boolean;
+  onClick?: React.MouseEventHandler<HTMLButtonElement> | undefined;
 }
 
 const StoreButton: React.FC<StoreButtonProps> = (props) => {
-  const { text, onClick } = props;
+  const { type = 'primary', size = 'large', text, disabled, onClick } = props;
+
   return (
-    <button className="store-button" type="button" onClick={onClick}>
+    <button className={`store-button ${disabled ? 'disabled' : type} ${size}`} type="button" onClick={onClick} disabled={disabled}>
       {text}
     </button>
   );
