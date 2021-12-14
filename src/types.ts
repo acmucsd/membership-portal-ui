3687a5a5dfadf8f151c70b0a71758a65db80f281
Sean Ye <seanye24@gmail.com>
2021-12-13 18:20:25 -0800

    Update types

--- src/types.ts
+++ src/types.ts
@@ -38,7 +38,7 @@ export interface PublicMerchItemOption {
   price: number;
   quantity?: number;
   discountPercentage: number;
-  metadata: MerchItemOptionMetadata;
+  metadata: MerchItemOptionMetadata | null;
 }
 
 export interface MerchItemOptionMetadata {
