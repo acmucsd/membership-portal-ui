edd540f00b85dcf905507e3c4c6b50246d8eb24a
Steven Steiner <steets250@gmail.com>
2021-12-16 18:05:45 -0800

    StorePage Admin

--- src/store/components/StorePage/index.tsx
+++ src/store/components/StorePage/index.tsx
@@ -1,20 +1,26 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import { fetchCollections } from '../../storeActions';
 import { notify } from '../../../utils';
 import { PublicMerchCollection } from '../../../types';
 
+import EditableIcon from '../../../assets/icons/editable-icon.svg';
 import StoreHeader from '../StoreHeader';
 import ItemCard from '../ItemCard';
 
 import './style.less';
+import StoreButton from '../StoreButton';
 
 interface StorePageProps {
   fetchCollections: Function;
+  isAdmin: boolean;
 }
 
 const StorePage: React.FC<StorePageProps> = (props) => {
+  const { isAdmin } = props;
+
   const [collections, setCollections] = useState<PublicMerchCollection[]>([]);
 
   useEffect(() => {
@@ -32,17 +38,28 @@ const StorePage: React.FC<StorePageProps> = (props) => {
     <>
       <StoreHeader showBalance showCart />
       <div className="store-page">
+        {isAdmin && (
+          <Link to="/store/admin/collection">
+            <StoreButton type="secondary" size="large" text="Create Collection" />
+          </Link>
+        )}
         <div className="collections">
           {collections.map((collection) => {
             return (
               <div className="collection" key={collection.uuid} id={collection.uuid}>
                 <h2 className="collection-header" style={{ color: collection.themeColorHex }}>
                   {collection.title}
+                  {isAdmin && (
+                    <Link to={`/store/admin/collection/${collection.uuid}`}>
+                      <img className="collection-header-editable-icon" src={EditableIcon} alt="Editable" />
+                    </Link>
+                  )}
                 </h2>
                 <div className="collection-items">
                   {collection.items.map((item, index) => (
-                    <ItemCard item={item} key={index} />
+                    <ItemCard item={item} key={index} editable={isAdmin} editableLink={`/store/admin/item/${item.uuid}`} />
                   ))}
+                  {isAdmin && <ItemCard placeholder placeholderLink="/store/admin/item" />}
                 </div>
               </div>
             );
@@ -53,4 +70,8 @@ const StorePage: React.FC<StorePageProps> = (props) => {
   );
 };
 
-export default connect(() => ({}), { fetchCollections })(StorePage);
+const mapStateToProps = (state: { [key: string]: any }) => ({
+  isAdmin: state.auth.admin,
+});
+
+export default connect(mapStateToProps, { fetchCollections })(StorePage);
