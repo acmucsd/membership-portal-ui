ef44d421b580f6d2871a412f4dc9818897a5dc98
Steven Steiner <steets250@gmail.com>
2021-12-18 00:56:29 -0800

    StoreCheckbox

--- src/store/components/StoreCheckbox/index.tsx
+++ src/store/components/StoreCheckbox/index.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
+import { Checkbox } from 'antd';
 
 import './style.less';
 
 interface StoreCheckboxProps {
-  value?: string | ReadonlyArray<string> | number;
+  checked?: boolean;
+  disabled?: boolean;
+  onChange?: (value: boolean) => void;
 }
 
 const StoreCheckbox: React.FC<StoreCheckboxProps> = (props) => {
-  const { value } = props;
+  const { checked, disabled, onChange } = props;
 
-  return <input className="store-checkbox" type="checkbox" value={value} />;
+  return <Checkbox className="store-checkbox" checked={checked} disabled={disabled} onChange={(e) => onChange && onChange(e.target.value)} />;
 };
 
 export default StoreCheckbox;
