296690300150a96a5771bc49981798f84c3b23f7
Steven Steiner <steets250@gmail.com>
2021-12-19 22:50:00 -0800

    Linting

--- src/store/components/CartPage/index.tsx
+++ src/store/components/CartPage/index.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Button, Typography } from 'antd';
+import { Typography } from 'antd';
 
 import CartDisplay from '../CartDisplay';
 import StoreHeader from '../StoreHeader';
