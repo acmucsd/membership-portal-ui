3dbb62b95a0041bc77fa69fd37dd7826c56eb719
Ronak Shah <rmshah@ucsd.edu>
2021-12-20 22:22:22 -0800

    Added almost all of the fields for create item (#496)

--- src/store/components/StoreCheckbox/index.tsx
+++ src/store/components/StoreCheckbox/index.tsx
@@ -2,17 +2,19 @@ import React from 'react';
 import { Checkbox } from 'antd';
 
 import './style.less';
+import { CheckboxChangeEvent } from 'antd/lib/checkbox';
 
 interface StoreCheckboxProps {
+  attributeName?: string;
   checked?: boolean;
   disabled?: boolean;
-  onChange?: (value: boolean) => void;
+  onChange?: (value: CheckboxChangeEvent) => void;
 }
 
 const StoreCheckbox: React.FC<StoreCheckboxProps> = (props) => {
-  const { checked, disabled, onChange } = props;
+  const { checked, disabled, onChange, attributeName } = props;
 
-  return <Checkbox className="store-checkbox" checked={checked} disabled={disabled} onChange={(e) => onChange && onChange(e.target.value)} />;
+  return <Checkbox name={attributeName} className="store-checkbox" checked={checked} disabled={disabled} onChange={(e) => onChange?.(e)} />;
 };
 
 export default StoreCheckbox;
