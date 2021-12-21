7494eaebaddc8eea497c9b121e141b4e2a9d8608
Ronak Shah <ronakshahultimate@gmail.com>
2021-12-20 21:42:36 -0800

    added orders and order page

--- src/store/components/OrderPage/index.tsx
+++ src/store/components/OrderPage/index.tsx
@@ -1,6 +1,9 @@
+import moment from 'moment';
 import React from 'react';
 
-import { PublicOrder } from '../../../types';
+import { CartItem, PublicMerchCollection, PublicOrder } from '../../../types';
+import CartDisplay from '../CartDisplay';
+import StoreHeader from '../StoreHeader';
 
 import './style.less';
 
@@ -11,7 +14,46 @@ interface OrderPageProps {
 const OrderPage: React.FC<OrderPageProps> = (props) => {
   const { order } = props;
 
-  return <div className="order-page">Order Page, order={JSON.stringify(order)}</div>;
+  return (
+    <>
+      <StoreHeader breadcrumb breadcrumbLocation="../orders" />
+      <div className="order-page">
+        <h1>Order</h1>
+        <h3>Purchase Date: {moment(order?.orderedAt).format('MMMM Do, YYYY')}</h3>
+        <h3 className="emphasized">
+          Pickup Event:{' '}
+          {moment(order?.pickupEvent.start).format('MMMM Do, YYYY [from] h:mm a [to] ') + moment(order?.pickupEvent.end).format('h:mm a ')}
+          at {order?.pickupEvent.title}
+        </h3>
+
+        <CartDisplay
+          writable={false}
+          items={
+            order?.items.map(
+              (publicItem): CartItem => {
+                const returnType: CartItem = {
+                  option: publicItem.option,
+                  quantity: publicItem.option.quantity || 1,
+                  item: {
+                    ...publicItem,
+                    itemName: 'missing',
+                    collection: {} as PublicMerchCollection,
+                    description: 'missing',
+                    picture: 'https://http.cat/404',
+                    monthlyLimit: -1,
+                    lifetimeLimit: -1,
+                    hasVariantsEnabled: false,
+                    options: [publicItem.option],
+                  },
+                };
+                return returnType;
+              },
+            ) ?? []
+          }
+        />
+      </div>
+    </>
+  );
 };
 
 export default OrderPage;
--- src/store/components/OrdersPage/index.tsx
+++ src/store/components/OrdersPage/index.tsx
@@ -1,6 +1,9 @@
+import moment from 'moment';
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import { PublicOrder } from '../../../types';
+import StoreHeader from '../StoreHeader';
 
 import './style.less';
 
@@ -11,7 +14,22 @@ interface OrdersPageProps {
 const OrdersPage: React.FC<OrdersPageProps> = (props) => {
   const { orders } = props;
 
-  return <div className="orders-page">Orders Page, orders={JSON.stringify(orders)}</div>;
+  return (
+    <>
+      <StoreHeader />
+      {orders?.map((order) => {
+        return (
+          <div className="order-thing">
+            <Link to={`./order/${order.uuid}`}>
+              <h1>Order (Status Unknown)</h1>
+            </Link>
+            <h3>Purchase Date: {moment(order.orderedAt).format('MMMM Do, YYYY')}</h3>
+            <h3>Pickup Date: {moment(order.pickupEvent.start).format('MMMM Do, YYYY')}</h3>
+          </div>
+        );
+      })}
+    </>
+  );
 };
 
 export default OrdersPage;
