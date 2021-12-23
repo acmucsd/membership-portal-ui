ea04d29a123d847d55cf3822394c1200c3b3ff59
Ronak Shah <ronakshahultimate@gmail.com>
2021-12-22 22:41:05 -0800

    added the page

--- src/store/components/AdminPreparePage/index.tsx
+++ src/store/components/AdminPreparePage/index.tsx
@@ -1,3 +1,5 @@
+import { Table } from 'antd';
+import moment from 'moment';
 import React, { useState } from 'react';
 
 import { PublicOrderPickupEvent } from '../../../types';
@@ -37,10 +39,90 @@ const AdminPreparePage: React.FC<AdminPreparePageProps> = (props) => {
     );
   }
 
+  const merchData: Record<string, { quantity: number; name: string; variantType: string; variantValue: string }> = {};
+  pickupEvent.orders?.forEach((order) => {
+    order.items.forEach((item) => {
+      merchData[`${item.uuid}${item.option.metadata?.type}${item.option.metadata?.value}`] = merchData[
+        `${item.uuid}${item.option.metadata?.type}${item.option.metadata?.value}`
+      ] ?? {
+        quantity: 0,
+        name: item.uuid,
+        variantType: item.option.metadata?.type,
+        variantValue: item.option.metadata?.value,
+      };
+      merchData[`${item.uuid}${item.option.metadata?.type}${item.option.metadata?.value}`].quantity += item.option.quantity;
+    });
+  });
+
   return (
     <>
       <StoreHeader breadcrumb breadcrumbLocation="/store/admin/prepare" />
-      <div className="admin-prepare-page">Admin Prepare Page, pickupEvent={JSON.stringify(pickupEvent)}</div>
+      <div className="admin-prepare-page">
+        <h1 className="admin-prepare-page-title">Prepare Orders</h1>
+        <h3 className="admin-prepare-page-subtitle">
+          Pickup Event: {moment(pickupEvent.start).format('dddd, MMMM Do, YYYY [from] ha [to] ')}
+          {moment(pickupEvent.end).format('ha')} at Geisel
+        </h3>
+        <h2 className="admin-prepare-page-secondary">Summary of Items</h2>
+        <Table
+          pagination={false}
+          className="admin-prepare-page-table"
+          size="small"
+          dataSource={Object.values(merchData).map((data) => ({
+            itemDisplay: (
+              <>
+                <h2>{data.name}</h2>
+                <h3>
+                  {data.variantType}: {data.variantValue}
+                </h3>
+              </>
+            ),
+            quantity: data.quantity,
+          }))}
+          columns={[
+            {
+              title: 'Item',
+              dataIndex: 'itemDisplay',
+              key: 'itemDisplay',
+            },
+            {
+              title: 'Quantity',
+              dataIndex: 'quantity',
+              key: 'quantity',
+            },
+          ]}
+        />
+        <h2 className="admin-prepare-page-secondary">User Breakdown</h2>
+        <Table
+          pagination={false}
+          className="admin-prepare-page-table"
+          size="small"
+          dataSource={pickupEvent.orders?.map((event) => ({
+            user: event.user,
+            items: (
+              <ul>
+                {event.items.map((curr) => (
+                  <li>
+                    Item Name Lost ({curr.option.metadata?.type}: {curr.option.metadata?.value}) x {curr.option.quantity}
+                  </li>
+                ))}
+              </ul>
+            ),
+          }))}
+          columns={[
+            {
+              title: 'User',
+              dataIndex: 'user',
+              key: 'user',
+            },
+            {
+              title: 'Items',
+              dataIndex: 'items',
+              key: 'items',
+            },
+          ]}
+        />
+      </div>
     </>
   );
 };
