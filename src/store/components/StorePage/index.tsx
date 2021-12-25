e8ff5f5106aa0615dd7525d3005fc70076da339b
Steven Steiner <steets250@gmail.com>
2021-12-25 13:55:39 -0800

    Update index.tsx

--- src/store/components/StorePage/index.tsx
+++ src/store/components/StorePage/index.tsx
@@ -1,71 +1,22 @@
-import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react';
 import { connect } from 'react-redux';
 
 import { fetchCollections } from '../../storeActions';
-import { notify } from '../../../utils';
-import { PublicMerchCollection } from '../../../types';
 
-import EditableIcon from '../../../assets/icons/editable-icon.svg';
 import StoreHeader from '../StoreHeader';
-import ItemCard from '../ItemCard';
 
 import './style.less';
-import StoreButton from '../StoreButton';
 
 interface StorePageProps {
   fetchCollections: Function;
   isAdmin: boolean;
 }
 
-const StorePage: React.FC<StorePageProps> = (props) => {
-  const { isAdmin } = props;
-
-  const [collections, setCollections] = useState<PublicMerchCollection[]>([]);
-
-  useEffect(() => {
-    props
-      .fetchCollections()
-      .then((value) => {
-        setCollections(value);
-      })
-      .catch((reason) => {
-        notify('API Error', reason.message || reason);
-      });
-  }, [props]);
-
+const StorePage: React.FC<StorePageProps> = () => {
   return (
     <>
       <StoreHeader showBalance showCart />
-      <div className="store-page">
-        {isAdmin && <StoreButton type="secondary" size="large" text="Create Collection" link="/store/admin/collection" />}
-        <div className="collections">
-          {collections.map((collection) => {
-            if (collection.items.length !== 0 || isAdmin) {
-              return (
-                <div className="collection" key={collection.uuid} id={collection.uuid}>
-                  <h2 className="collection-header" style={{ color: collection.themeColorHex }}>
-                    {collection.title}
-                    {isAdmin && (
-                      <Link to={`/store/admin/collection/${collection.uuid}`}>
-                        <img className="collection-header-editable-icon" src={EditableIcon} alt="Editable" />
-                      </Link>
-                    )}
-                  </h2>
-                  <div className="collection-items">
-                    {collection.items.map((item, index) => (
-                      <ItemCard item={item} key={index} editable={isAdmin} editableLink={`/store/admin/item/${item.uuid}`} />
-                    ))}
-                    {isAdmin && <ItemCard placeholder placeholderLink="/store/admin/item" />}
-                  </div>
-                </div>
-              );
-            }
-
-            return null;
-          })}
-        </div>
-      </div>
+      <div className="store-page">Coming 2024</div>
     </>
   );
 };
