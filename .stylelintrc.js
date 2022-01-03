58cff03f5d4998d6eb53eee88cf59ff71c82133a
Steven Steiner <steets250@gmail.com>
2022-01-02 19:57:19 -0800

    Lint Existing Files

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -5,6 +5,7 @@ module.exports = {
   rules: {
     'order/order': ['custom-properties', 'declarations'],
     'order/properties-alphabetical-order': true,
-    'font-weight-notation': 'numeric'
+    'font-weight-notation': 'numeric',
+    'selector-class-pattern': '^([a-zA-z][a-zA-z0-9]*)([\-\_]+[a-zA-z0-9]+)*$' 
   },
 };
