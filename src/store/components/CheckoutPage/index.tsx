d8c47d05ff9a208f2d77e5672b970ca9a0347fa5
Paul Pan <panjunhong05@gmail.com>
2022-01-04 13:03:33 -0800

    Checkout page completion.

--- src/store/components/CheckoutPage/index.tsx
+++ src/store/components/CheckoutPage/index.tsx
@@ -1,18 +1,31 @@
 import React, { useEffect, useState } from 'react';
 import { CartItem } from '../../../types';
 import CartDisplay from '../CartDisplay';
+import Config from '../../../config';
+import { history } from '../../../redux_store';
 import StoreButton from '../StoreButton';
 import StoreDropdown from '../StoreDropdown';
 import StoreHeader from '../StoreHeader';
+import { fetchService, notify } from '../../../utils';
 
 type CheckoutPageProps = {
   cart: CartItem[];
   getFuturePickup: (onFail: () => void) => Promise<any>;
 };
 
+interface ItemAPIData {
+  option: string;
+  quantity: number;
+}
+
+interface APIData {
+  order: ItemAPIData[];
+  pickupEvent: string;
+}
+
 const CheckoutPage: React.FC<CheckoutPageProps> = ({ cart, getFuturePickup }) => {
   const [pickupEvents, setPickupEvents] = useState({});
-  const [eventUUID, setEventUUID] = useState(undefined);
+  const [eventUUID, setEventUUID] = useState('');
   useEffect(() => {
     const updateEvents = async () => {
       const resultEvents = await getFuturePickup(() => {});
@@ -31,7 +44,30 @@ const CheckoutPage: React.FC<CheckoutPageProps> = ({ cart, getFuturePickup }) =>
             setEventUUID(pickupEvents[option.value]);
           }}
         />
-        <StoreButton text="Place Order" disabled={eventUUID === undefined} />
+        <StoreButton
+          text="Place Order"
+          disabled={eventUUID === ''}
+          onClick={async () => {
+            const inputData: APIData = { order: [], pickupEvent: eventUUID };
+            cart.forEach((item) => {
+              inputData.order.push({ option: item.option.uuid, quantity: item.quantity });
+            });
+            const url = `${Config.API_URL}${Config.routes.store.order}`;
+            try {
+              const result = await fetchService(url, 'POST', 'json', {
+                requiresAuthorization: true,
+                payload: JSON.stringify(inputData),
+              });
+              if (result.error !== undefined || result.error !== null) {
+                throw result.error;
+              }
+              history.push(`/store/${result.order.uuid}`);
+            } catch (error) {
+              notify('Order placement error', error.message);
+              history.push('/store/cart');
+            }
+          }}
+        />
       </div>
     </>
   );
