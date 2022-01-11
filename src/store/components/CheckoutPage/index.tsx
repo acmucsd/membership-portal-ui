44c4c90e7009515b58b12757c010cbf9a47cc5af
Steven Steiner <steets250@gmail.com>
2022-01-10 16:24:53 -0800

    Remove Confetti

--- src/store/components/CheckoutPage/index.tsx
+++ src/store/components/CheckoutPage/index.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import moment from 'moment';
-import confetti from 'canvas-confetti';
 import { CartItem, PublicOrderPickupEvent } from '../../../types';
 import CartDisplay from '../CartDisplay';
 import Config from '../../../config';
@@ -66,26 +65,6 @@ const CheckoutPage: React.FC<CheckoutPageProps> = ({ cart, getFuturePickup, clea
                 payload: JSON.stringify(inputData),
               });
               clearCart();
-              const duration = 3 * 1000;
-              const animationEnd = Date.now() + duration;
-              const defaults = { startVelocity: 30, spread: 360, ticks: 120, zIndex: 0 };
-
-              const randomInRange = (min, max) => {
-                return Math.random() * (max - min) + min;
-              };
-
-              const interval = setInterval(() => {
-                const timeLeft = animationEnd - Date.now();
-
-                if (timeLeft <= 0) {
-                  return clearInterval(interval);
-                }
-
-                const particleCount = 50 * (timeLeft / duration);
-                // since particles fall down, start a bit higher than random
-                confetti({ ...defaults, particleCount, origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 } });
-                confetti({ ...defaults, particleCount, origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 } });
-              }, 250);
               history.push(`/store/order/${result.order.uuid}`);
             } catch (error) {
               notify('Order placement error', error.message);
