d855e094d3fe19fa220b8beffb96a2304f135c27
Trevor Kwan <trevorkw7@gmail.com>
2022-01-27 17:20:05 -0800

    Added Switch

--- src/layout/components/Header/index.tsx
+++ src/layout/components/Header/index.tsx
@@ -3,12 +3,20 @@ import { NavLink } from 'react-router-dom';
 
 import NavDropdown from '../../containers/NavDropdown';
 import NavProfile from '../../containers/NavProfile';
-
+import Icon from '@ant-design/icons';
 import logo from '../../../assets/graphics/logo.png';
-
+import SunIcon from '../../../assets/graphics/logo.png';
+import MoonIcon from '../../../assets/graphics/logo.png';
+import './custom.less';
+import { Switch } from 'antd';
 import './style.less';
+import { addLeadingSlash } from 'history/PathUtils';
 
 const Header: React.FC = () => {
+  var less = require("less");
+  const HandleModeSwitch = () => {
+    less.modifyVars({ "theme": "dark" });
+  }
   return (
     <div className="header-container">
       <div className="header-content">
@@ -17,6 +25,7 @@ const Header: React.FC = () => {
             <img className="logo" src={logo} alt="ACM Logo" />
             <span className="subheading">&nbsp;Membership Portal</span>
           </NavLink>
+          <Switch onClick={HandleModeSwitch} className="mode-switch"/>
           <div className="profile">
             <NavProfile menu={<NavDropdown />} />
           </div>
