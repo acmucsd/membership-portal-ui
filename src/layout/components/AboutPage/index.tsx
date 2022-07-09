d6b6e7c103a6079da31eac382b829fd309a0e27f
Steven Steiner <steets250@gmail.com>
2022-07-09 14:03:47 -0700

    Remove About Page Config

--- src/layout/components/AboutPage/index.tsx
+++ src/layout/components/AboutPage/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import Icon from '@ant-design/icons';
 
 import './style.less';
-import config from '../../../config';
 import logo from '../../../assets/graphics/logo.png';
 import { ReactComponent as DiscordIcon } from '../../../assets/icons/discord-icon.svg';
 import { ReactComponent as FbIcon } from '../../../assets/icons/fb-icon.svg';
@@ -18,7 +17,16 @@ const AboutPage: React.FC = () => {
           About <b>ACM@UCSD</b>
         </h1>
       </div>
-      <p className="description">{config.about}</p>
+      <p className="description">
+        {`With 100,000 members and 500+ chapters, the Association for Computing
+          Machinery is the world's largest society for computing. Here at UC
+          San Diego, our chapter has been established with the mission of
+          creating a member-first community devoted to the field of computing.
+          We welcome students of all backgrounds and skill levels to come
+          develop their skills at our many workshops and form new friendships at
+          our many socials. Get involved today by signing up for an event on
+          this portal or following us on social media!`}
+      </p>
       <div className="socials">
         <a href="https://acmucsd.com">
           <Icon component={WebIcon} className="icon" />
