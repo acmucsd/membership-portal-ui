e4cd373a099b663600c5bcfecd4aceb444ffe51c
WishingWell13 <andy.smithwick@gmail.com>
2023-04-12 20:01:31 -0700

    Filters still not working (#609)
    * Filters still not working
    
    Admin Resume Table Sorting in progress
    
    * Fixed prettier issues
    
    * Fixed more prettier issues
    
    * Removed unused bits of code and old comments
    
    * Removed test data, begun default value changing
    
    * Filters for graduation year works
    
    * Removed onFilterDropdownVisibleChange
    
    * Update src/admin/components/UserResumeTable/index.tsx
    
    Co-authored-by: Faris Ashai <farisashai@gmail.com>
    
    * Revert "Update src/admin/components/UserResumeTable/index.tsx"
    
    This reverts commit 44c4125575fc469c44ca7593a8718dc5f4fee05e.
    
    ---------
    
    Co-authored-by: Faris Ashai <farisashai@gmail.com>

--- src/admin/components/UserResumeTable/index.tsx
+++ src/admin/components/UserResumeTable/index.tsx
@@ -1,9 +1,10 @@
 // component for admin table to use inside of userresumetable component
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Table, Button, Avatar } from 'antd';
 import JSZip from 'jszip';
 import { saveAs } from 'file-saver';
 import './style.less';
+import { ColumnFilterItem } from 'antd/lib/table';
 import { getDefaultProfile } from '../../../utils';
 import { UserResume } from '../../../types';
 
@@ -11,9 +12,28 @@ interface UserResumeTableProps {
   resumes: UserResume[];
 }
 
+interface YearFilter extends ColumnFilterItem {
+  text: string;
+  value: string;
+}
+
 const UserResumeTable: React.FC<UserResumeTableProps> = (props) => {
   const { resumes } = props;
+  const [yearFilters, setYearFilters] = useState<YearFilter[]>([]);
   const [selectedRowKeys, setSelectedRowKeys] = useState<React.Key[]>([]);
+
+  const updateYearFilters = () => {
+    setYearFilters(
+      // Resumes.indexOf(value) === index checks for first occurance
+      resumes
+        ?.filter((value: UserResume, index: number) => resumes.indexOf(value) === index)
+        .sort((resume1, resume2) => resume1.user.graduationYear - resume2.user.graduationYear)
+        .map((element) => ({
+          text: `${element.user.graduationYear}`,
+          value: `${element.user.graduationYear}`,
+        })),
+    );
+  };
 
   const fileName = 'acm_resumes';
 
@@ -48,6 +68,10 @@ const UserResumeTable: React.FC<UserResumeTableProps> = (props) => {
     zip.generateAsync({ type: 'blob' }).then((blob) => saveAs(blob, filename));
   };
 
+  useEffect(() => {
+    updateYearFilters();
+  }, [resumes]);
+
   const handleDownloadZip = () => {
     const selectedResumeLinks: string[] = selectedRowKeys.map((key) => resumes.find((resume) => resume.uuid === key)?.url ?? '');
     saveZip(fileName, selectedResumeLinks);
@@ -71,6 +95,12 @@ const UserResumeTable: React.FC<UserResumeTableProps> = (props) => {
           </a>
         </div>
       ),
+
+      sorter: (record1: UserResume, record2: UserResume) => {
+        const fullName1 = `${record1.user.firstName} ${record1.user.lastName}`;
+        const fullName2 = `${record2.user.firstName} ${record2.user.lastName}`;
+        return fullName1.localeCompare(fullName2);
+      },
     },
     {
       title: 'Major',
@@ -79,11 +109,17 @@ const UserResumeTable: React.FC<UserResumeTableProps> = (props) => {
     {
       title: 'Planned Graduation',
       dataIndex: 'user.graduationYear',
+
+      filterMultiple: true,
+      filters: yearFilters, // [{ text: "2016", value: "2016" }],
+      // Value we are filtering by, record = content of current row
+      onFilter: (value: string, record: UserResume) => `${record.user.graduationYear}` === value,
     },
     {
       title: 'Date Uploaded',
       dataIndex: 'lastUpdated',
       render: (date: string) => <span>{reformatDate(date)}</span>,
+      sorter: (user1, user2) => user1.lastUpdated.localeCompare(user2.lastUpdated),
     },
   ];
 
